9cc0e706854194918b2ebb76f010521dcdd0929e
Luca Barbato <lu_zero@gentoo.org>
2014-07-25 00:47:21 +0200

    cppbuild: Expose the binary path
    Simplifies building autotools-based libraries

--- cppbuild.sh
+++ cppbuild.sh
@@ -72,14 +72,17 @@ fi
 export ANDROID_CPP="$ANDROID_NDK/sources/cxx-stl/gnu-libstdc++/${ANDROID_GCC_VERSION}/"
 case $PLATFORM in
     android-x86)
-        export ANDROID_BIN="$ANDROID_NDK/toolchains/x86-${ANDROID_GCC_VERSION}/prebuilt/$KERNEL-$ARCH/bin/i686-linux-android"
+        export ANDROID_BINDIR="$ANDROID_NDK/toolchains/x86-${ANDROID_GCC_VERSION}/prebuilt/$KERNEL-$ARCH/bin/i686-linux-android"
+        export ANDROID_CHOST="i686-linux-android"
         export ANDROID_ROOT="$ANDROID_NDK/platforms/android-9/arch-x86/"
         ;;
     *)
-        export ANDROID_BIN="$ANDROID_NDK/toolchains/arm-linux-androideabi-${ANDROID_GCC_VERSION}/prebuilt/$KERNEL-$ARCH/bin/arm-linux-androideabi"
+        export ANDROID_BINDIR="$ANDROID_NDK/toolchains/arm-linux-androideabi-${ANDROID_GCC_VERSION}/prebuilt/$KERNEL-$ARCH/bin/"
+        export ANDROID_CHOST="arm-linux-androideabi"
         export ANDROID_ROOT="$ANDROID_NDK/platforms/android-9/arch-arm/"
         ;;
 esac
+export ANDROID_BIN="${ANDROID_BINDIR}/${ANDROID_CHOST}"
 
 function download {
     COMMAND="curl -C - -L $1 -o $2"
