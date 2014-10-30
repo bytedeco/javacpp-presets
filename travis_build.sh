656641b715f77fe49973d21e81bdd2eb8384d771
Mark <github880520@mstier.de>
2014-10-31 00:15:21 +0100

    build for different ubuntu releases

--- travis_build.sh
+++ travis_build.sh
@@ -7,10 +7,10 @@ set -o pipefail
 
 export LC_ALL=C
 export TZ=UTC
-DISTNAME="precise"
+DISTNAME="${DISTNAME:-precise}"
 DISTURL="http://archive.ubuntu.com/ubuntu"
 DISTKEYRING="/usr/share/keyrings/ubuntu-archive-keyring.gpg"
-DISTARCH="amd64"
+DISTARCH="${DISTARCH:-amd64}"
 PROJECTS="opencv ffmpeg"
 BASEDIR="$(pwd)"
 TGTDIR="$BASEDIR/osinst.$DISTNAME.$DISTARCH"
