e457a7eac75016718ce9f92ecd724ea67dd43a4f
Mark <github880520@mstier.de>
2014-10-30 20:50:28 +0100

    add cmake

--- travis_build.sh
+++ travis_build.sh
@@ -121,7 +121,7 @@ deb $DISTURL $DISTNAME-security main restricted universe
 " > "/etc/apt/sources.list"
     apt-get update
     apt-get -y dist-upgrade
-    apt-get -y install openjdk-7-jdk maven build-essential curl
+    apt-get -y install openjdk-7-jdk maven build-essential curl cmake
     install_yasm
     useradd -u $freeuid -d /build build
     touch .installed
