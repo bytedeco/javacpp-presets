b06904c8d96cf8956a572a24a8fb4aae4cd32d0e
mark <email>
2016-02-22 12:51:28 +0100

    dito

--- .travis.sh
+++ .travis.sh
@@ -8,11 +8,15 @@ set -Eex
 set -o pipefail
 
 wget -c https://github.com/bytedeco/javacpp/archive/master.zip
+rm -rf javacpp-master
 unzip master.zip
 mvn clean install -f javacpp-master
 
 for project in $PROJECTS; do
   ./cppbuild.sh install $project
-  mvn install -pl $project -am -amd
+  mvn install -pl $project -am
+  if test -d $project-it; then
+    mvn install -pl $project-it
+  fi
 done
 
