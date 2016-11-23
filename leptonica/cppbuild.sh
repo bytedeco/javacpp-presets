51880df671495f3a243e44cfcf8b6bc2abbde2b1
vb216 <v.f.baines@gmail.com>
2016-11-23 06:23:39 +0000

    Update version of libpng (pull #321)

--- leptonica/cppbuild.sh
+++ leptonica/cppbuild.sh
@@ -10,14 +10,14 @@ fi
 ZLIB=zlib-1.2.8
 GIFLIB=giflib-5.1.4
 LIBJPEG=libjpeg-turbo-1.5.1
-LIBPNG=libpng-1.6.24
+LIBPNG=libpng-1.6.26
 LIBTIFF=tiff-4.0.6
 LIBWEBP=libwebp-0.5.1
 LEPTONICA_VERSION=1.73
 download http://zlib.net/$ZLIB.tar.gz $ZLIB.tar.gz
 download http://downloads.sourceforge.net/project/giflib/$GIFLIB.tar.gz $GIFLIB.tar.gz
 download http://downloads.sourceforge.net/project/libjpeg-turbo/1.5.1/$LIBJPEG.tar.gz $LIBJPEG.tar.gz
-download http://downloads.sourceforge.net/project/libpng/libpng16/1.6.24/$LIBPNG.tar.gz $LIBPNG.tar.gz
+download http://downloads.sourceforge.net/project/libpng/libpng16/1.6.26/$LIBPNG.tar.gz $LIBPNG.tar.gz
 download http://download.osgeo.org/libtiff/$LIBTIFF.tar.gz $LIBTIFF.tar.gz
 download http://downloads.webmproject.org/releases/webp/$LIBWEBP.tar.gz $LIBWEBP.tar.gz
 download http://www.leptonica.org/source/leptonica-$LEPTONICA_VERSION.tar.gz leptonica-$LEPTONICA_VERSION.tar.gz
