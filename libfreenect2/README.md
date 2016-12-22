5d954e08840845365fb3c0117e427f1d0bb2cd87
Jeremy Laviole <poqudrof@gmail.com>
2016-12-22 10:09:04 +0100

    updated sample usage.

--- libfreenect2/README.md
+++ libfreenect2/README.md
@@ -20,4 +20,125 @@ Java API documentation is available here:
 Sample Usage
 ------------
 
-No sample usage yet.
+Sample usage is in the `test/` folder. 
+
+Here is the full code: 
+
+* File: `src/main/java/freenect2Test.java`
+
+``` java
+import org.bytedeco.javacpp.Loader;
+import org.bytedeco.javacpp.freenect2;
+import org.bytedeco.javacpp.freenect2.CpuPacketPipeline;
+import org.bytedeco.javacpp.freenect2.FrameMap;
+import org.bytedeco.javacpp.freenect2.Freenect2;
+import org.bytedeco.javacpp.freenect2.Freenect2Device;
+import org.bytedeco.javacpp.freenect2.PacketPipeline;
+import org.bytedeco.javacpp.freenect2.SyncMultiFrameListener;
+
+public class freenect2Test {
+    public static void main(String[] args) {
+        Freenect2 freenect2Context;
+        try {
+            Loader.load(org.bytedeco.javacpp.freenect2.class);
+            freenect2Context = new Freenect2();
+        } catch (Exception e) {
+            System.out.println("Exception in the TryLoad !" + e);
+            return;
+        }
+        Freenect2Device device = null;
+        PacketPipeline pipeline = null;
+        String serial = "";
+
+	// Only CPU pipeline tested.
+        pipeline = new CpuPacketPipeline();
+//        pipeline = new libfreenect2::OpenGLPacketPipeline();
+//        pipeline = new libfreenect2::OpenCLPacketPipeline(deviceId);
+//        pipeline = new libfreenect2::CudaPacketPipeline(deviceId);
+
+        if (serial == "") {
+            serial = freenect2Context.getDefaultDeviceSerialNumber().getString();
+            System.out.println("Serial:" + serial);
+        }
+
+        device = freenect2Context.openDevice(serial, pipeline);
+        // [listeners]
+        int types = 0;
+        types |= freenect2.Frame.Color;
+        types |= freenect2.Frame.Ir | freenect2.Frame.Depth;
+
+        SyncMultiFrameListener listener = new freenect2.SyncMultiFrameListener(types);
+
+        device.setColorFrameListener(listener);
+        device.setIrAndDepthFrameListener(listener);
+
+        device.start();
+
+        System.out.println("Serial: " + device.getSerialNumber().getString());
+        System.out.println("Firmware: " + device.getFirmwareVersion().getString());
+
+        FrameMap frames = new FrameMap();
+
+        int frameCount = 0;
+        for (int i = 0; i < 100; i++) {
+            System.out.println("getting frame " + frameCount);
+            if (!listener.waitForNewFrame(frames, 10 * 1000)) // 10 sconds
+            {
+                System.out.println("timeout!");
+                return;
+            }
+
+            freenect2.Frame rgb = frames.get(freenect2.Frame.Color);
+            freenect2.Frame ir = frames.get(freenect2.Frame.Ir);
+            freenect2.Frame depth = frames.get(freenect2.Frame.Depth);
+
+            System.out.println("RGB image, w:" + rgb.width() + " " + rgb.height());
+            byte[] imgData = new byte[1000];
+            rgb.data().get(imgData);
+            for (int pix = 0; pix < 10; pix++) {
+                System.out.print(imgData[pix] + " ");
+            }
+            System.out.println();
+            frameCount++;
+            listener.release(frames);
+            continue;
+        }
+        device.stop();
+        device.close();
+    }
+}
+``` 
+
+
+* File: `pom.xml`
+
+```xml
+<project>
+    <modelVersion>4.0.0</modelVersion>
+    <groupId>org.bytedeco.javacpp-presets.libfreenect</groupId>
+    <artifactId>freenect2Test</artifactId>
+    <version>0.2.0</version>
+    <properties>
+        <exec.mainClass>freenect2Test</exec.mainClass>
+    </properties>
+    <dependencies>
+        <dependency>
+            <groupId>org.bytedeco.javacpp-presets</groupId>
+            <artifactId>libfreenect2</artifactId>
+            <version>0.2.0-1.3</version>
+        </dependency>
+        <dependency>
+          <groupId>org.jogamp.gluegen</groupId>
+          <artifactId>gluegen-rt-main</artifactId>
+          <version>2.3.1</version>
+          <optional>true</optional>
+        </dependency>
+        <dependency>
+          <groupId>org.jogamp.jogl</groupId>
+          <artifactId>jogl-all-main</artifactId>
+          <version>2.3.1</version>
+          <optional>true</optional>
+        </dependency>
+    </dependencies>
+</project>
+```
--- libfreenect2/test/src/main/java/freenect2Test.java
+++ libfreenect2/test/src/main/java/freenect2Test.java
@@ -1,166 +1,90 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+import org.bytedeco.javacpp.Loader;
+import org.bytedeco.javacpp.freenect2;
 import org.bytedeco.javacpp.freenect2.CpuPacketPipeline;
+import org.bytedeco.javacpp.freenect2.FrameMap;
 import org.bytedeco.javacpp.freenect2.Freenect2;
 import org.bytedeco.javacpp.freenect2.Freenect2Device;
 import org.bytedeco.javacpp.freenect2.PacketPipeline;
+import org.bytedeco.javacpp.freenect2.SyncMultiFrameListener;
 
 /**
  *
- * @author jiii
+ * @author Jeremy Laviole
  */
 public class freenect2Test {
 
     public static void main(String[] args) {
+        Freenect2 freenect2Context;
+        try {
+            Loader.load(org.bytedeco.javacpp.freenect2.class);
+            // Context is shared accross cameras.
+            freenect2Context = new Freenect2();
 
-        Freenect2 freenect2 = new Freenect2();
+        } catch (Exception e) {
+            System.out.println("Exception in the TryLoad !" + e);
+            e.printStackTrace();
+            return;
+        }
         Freenect2Device device = null;
         PacketPipeline pipeline = null;
-/// [context]
-//  libfreenect2::Freenect2 freenect2;
-//  libfreenect2::Freenect2Device *dev = 0;
-//  libfreenect2::PacketPipeline *pipeline = 0;
-/// [context]
         String serial = "";
-
-        boolean viewer_enabled = true;
-        boolean enable_rgb = true;
-        boolean enable_depth = true;
-        int deviceId = -1;
-        int framemax = -1;
 
         pipeline = new CpuPacketPipeline();
 //        pipeline = new libfreenect2::OpenGLPacketPipeline();
 //        pipeline = new libfreenect2::OpenCLPacketPipeline(deviceId);
 //        pipeline = new libfreenect2::CudaPacketPipeline(deviceId);
 
-        if (freenect2.enumerateDevices() == 0) {
-            System.out.println("no device connected!");
-            return;
-        }
-
         if (serial == "") {
-            serial = freenect2.getDefaultDeviceSerialNumber().getString();
+            serial = freenect2Context.getDefaultDeviceSerialNumber().getString();
             System.out.println("Serial:" + serial);
         }
-/// [discovery]
 
-//        if (pipeline) {
-/// [open]
-        device = freenect2.openDevice(serial, pipeline);
-/// [open]
-//        } else {
-//            dev = freenect2.openDevice(serial);
-//        }
+        device = freenect2Context.openDevice(serial, pipeline);
+        // [listeners]
+        int types = 0;
+        types |= freenect2.Frame.Color;
+        types |= freenect2.Frame.Ir | freenect2.Frame.Depth;
 
-        if (device == null) {
-            System.out.println("failure opening device!");
-        } 
+        SyncMultiFrameListener listener = new freenect2.SyncMultiFrameListener(types);
 
-///// [listeners]
-//        int types = 0;
-//        if (enable_rgb) {
-//            types |= libfreenect2::Frame::Color;
-//        }
-//        if (enable_depth) {
-//            types |= libfreenect2::Frame::Ir | libfreenect2::Frame::Depth;
-//        }
-//        libfreenect2::SyncMultiFrameListener listener(types);
-//        libfreenect2::FrameMap frames;
-//        dev -> setColorFrameListener( & listener);
-//        dev -> setIrAndDepthFrameListener( & listener);
-///// [listeners]
-//
-///// [start]
-//        if (enable_rgb && enable_depth) {
-//            if (!dev -> start()) {
-//                return -1;
-//            }
-//        } else {
-//            if (!dev -> startStreams(enable_rgb, enable_depth)) {
-//                return -1;
-//            }
-//        }
-//        std::cout << "device serial: " << dev -> getSerialNumber() << std::endl;
-//        std::cout << "device firmware: " << dev -> getFirmwareVersion() << std::endl;
-///// [start]
-//
-///// [registration setup]
-//        libfreenect2::Registration * registration = new libfreenect2::Registration(dev -> getIrCameraParams(), dev -> getColorCameraParams());
-//  libfreenect2::Frame undistorted(512, 424, 4)
-//        , registered(512, 424, 4);
-///// [registration setup]
-//
-//        size_t framecount = 0;
-//        #
-//        ifdef EXAMPLES_WITH_OPENGL_SUPPORT
-//        Viewer viewer;
-//        if (viewer_enabled) {
-//            viewer.initialize();
-//        }
-//        #else
-//  viewer_enabled = false;
-//        #endif /// [loop start]
-//        while (!protonect_shutdown && (framemax == (size_t) - 1 || framecount < framemax)) {
-//            if (!listener.waitForNewFrame(frames, 10 * 1000)) // 10 sconds
-//            {
-//                std::cout << "timeout!" << std::endl;
-//                return -1;
-//            }
-//            libfreenect2::Frame * rgb = frames[libfreenect2::Frame::Color];
-//            libfreenect2::Frame * ir = frames[libfreenect2::Frame::Ir];
-//            libfreenect2::Frame * depth = frames[libfreenect2::Frame::Depth];
-///// [loop start]
-//
-//            if (enable_rgb && enable_depth) {
-//                /// [registration]
-//                registration -> apply(rgb, depth,  & undistorted,  & registered);
-///// [registration]
-//            }
-//
-//            framecount++;
-//            if (!viewer_enabled) {
-//                if (framecount % 100 == 0) {
-//                    std::cout << "The viewer is turned off. Received " << framecount << " frames. Ctrl-C to stop." << std::endl;
-//                }
-//                listener.release(frames);
-//                continue;
-//            }
-//            #
-//            ifdef EXAMPLES_WITH_OPENGL_SUPPORT
-//            if (enable_rgb) {
-//                viewer.addFrame("RGB", rgb);
-//            }
-//            if (enable_depth) {
-//                viewer.addFrame("ir", ir);
-//                viewer.addFrame("depth", depth);
-//            }
-//            if (enable_rgb && enable_depth) {
-//                viewer.addFrame("registered",  & registered);
-//            }
-//
-//            protonect_shutdown = protonect_shutdown || viewer.render();
-//            #
-//            endif /// [loop end]
-//                    listener
-//            .release(frames);
-//            /**
-//             * libfreenect2::this_thread::sleep_for(libfreenect2::chrono::milliseconds(100));
-//             */
-//        }
-//        /// [loop end]
-//
-//        // TODO: restarting ir stream doesn't work!
-//        // TODO: bad things will happen, if frame listeners are freed before dev->stop() :(
-//        /// [stop]
+        device.setColorFrameListener(listener);
+        device.setIrAndDepthFrameListener(listener);
+
+        device.start();
+
+        System.out.println("Serial: " + device.getSerialNumber().getString());
+        System.out.println("Firmware: " + device.getFirmwareVersion().getString());
+/// [start]
+
+        FrameMap frames = new FrameMap();
+        // Fetch 100 frames. 
+        int frameCount = 0;
+        for (int i = 0; i < 100; i++) {
+            System.out.println("getting frame " + frameCount);
+            if (!listener.waitForNewFrame(frames, 10 * 1000)) // 10 sconds
+            {
+                System.out.println("timeout!");
+                return;
+            }
+
+            freenect2.Frame rgb = frames.get(freenect2.Frame.Color);
+            freenect2.Frame ir = frames.get(freenect2.Frame.Ir);
+            freenect2.Frame depth = frames.get(freenect2.Frame.Depth);
+/// [loop start]
+            System.out.println("RGB image, w:" + rgb.width() + " " + rgb.height());
+            byte[] imgData = new byte[1000];
+            rgb.data().get(imgData);
+            for (int pix = 0; pix < 10; pix++) {
+                System.out.print(imgData[pix] + " ");
+            }
+            System.out.println();
+            frameCount++;
+            listener.release(frames);
+            continue;
+        }
+
         device.stop();
         device.close();
-//        delete registration;
-
-        return;
     }
 
 }
