aff5753ea8170c5c192407bd4e89af4ee8cd87d0
vb216 <ubuntu@ip-172-31-31-125.eu-west-2.compute.internal>
2017-03-22 16:28:28 +0000

    test

--- tensorflow/cppbuild.sh
+++ tensorflow/cppbuild.sh
@@ -75,6 +75,7 @@ case $PLATFORM in
 esac
 
 ./configure
+echo "Starting bazel step.."
 bazel build -c opt //tensorflow:libtensorflow_cc.so $BUILDFLAGS --spawn_strategy=standalone --genrule_strategy=standalone --verbose_failures
 
 case $PLATFORM in
