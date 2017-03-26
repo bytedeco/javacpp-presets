630994e972b718238e592ebee029d0bced994f84
vb216 <ubuntu@ip-172-31-31-125.eu-west-2.compute.internal>
2017-03-26 09:12:38 +0000

    test

--- prog.sh
+++ prog.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Using prog script for $1 project.."
-while true; do echo .; tail -10 ~/javacpp-presets/buildlogs/$1.log; sleep 300; done
+while true; do echo .; tail -10 ~/build/vb216/javacpp-presets/buildlogs/$1.log; sleep 300; done
 
