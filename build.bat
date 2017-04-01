862305abeeaf0bf57abe8c63db947da39393c411
vb216 <ubuntu@ip-172-31-31-125.eu-west-2.compute.internal>
2017-04-01 21:18:47 +0000

    test 32

--- build.bat
+++ build.bat
@@ -74,11 +74,21 @@
 
     IF "%projectName%"=="hdf5" ( 
        @echo on
-       curl.exe -L -o hdf5.zip "https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-1.10/hdf5-1.10.0-patch1/bin/windows/hdf5-1.10.0-patch1-win64-vs2013-shared.zip"
-       dir
-       unzip hdf5.zip 
-       cd hdf5
-       msiexec /i HDF5-1.10.0-win64.msi /quiet
+       IF "%MSYS2_ARCH%"=="x86_64" (
+          curl.exe -L -o hdf5.zip "https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-1.10/hdf5-1.10.0-patch1/bin/windows/hdf5-1.10.0-patch1-win64-vs2013-shared.zip"
+          unzip hdf5.zip 
+          cd hdf5
+          msiexec /i HDF5-1.10.0-win64.msi /quiet
+       )
+
+       IF "%MSYS2_ARCH%"=="x86" (
+          curl.exe -L -o hdf5.zip "https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-1.10/hdf5-1.10.0-patch1/bin/windows/hdf5-1.10.0-patch1-win32-vs2013-shared.zip"
+          unzip hdf5.zip 
+          cd hdf5
+          dir 
+          msiexec /i HDF5-1.10.0-win32.msi /quiet
+       )
+
        cd ..
        echo Finished hd5 install 
     )
