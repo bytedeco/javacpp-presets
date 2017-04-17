52797da0b52cd27871e1a5397a7401daceddbda9
vb216 <ubuntu@ip-172-31-31-125.eu-west-2.compute.internal>
2017-04-17 13:50:44 +0000

    use small wrapper to get vc vars

---
+++ ci/buildWrap.cmd
@@ -0,0 +1,18 @@
+@echo off
+set PROJ=%~1
+set DROPAUTH=%~2
+set CI_DEPLOY_USERNAME=%~3
+set CI_DEPLOY_PASSWORD=%~4
+cd %APPVEYOR_BUILD_FOLDER%
+
+IF "%MSYS2_ARCH%"=="x86_64" (
+   echo Callings vcvarsall for amd64
+   call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
+)
+IF "%MSYS2_ARCH%"=="x86" (
+   echo Callings vcvarsall for x86
+   call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
+)
+
+bash -lc"/c/projects/javacpp-presets/ci/build.sh %PROJ% %DROP_AUTH_TOK% %CI_DEPLOY_USERNAME% %CI_DEPLOY_PASSWORD%"
+
