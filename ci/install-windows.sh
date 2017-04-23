88bd9c426cd7ce26b1eb626d5f160290f8ed7538
vb216 <ubuntu@ip-172-31-31-125.eu-west-2.compute.internal>
2017-04-23 19:03:40 +0000

    rename install script, hdf5 copy and build flags for leptonic win32

--- ci/install-windows.sh
+++ ci/install-windows.sh
@@ -69,7 +69,7 @@ if [[ "$projectName" =~ "hdf5" ]]; then
           unzip hdf5.zip 
           cd hdf5
           msiexec //i HDF5-1.10.0-win32.msi //quiet
-          cp -R /c/Program\ Files\ \(x86\)/HDF_Group /c/Program\ Files
+          mv /c/Program\ Files\ \(x86\)/HDF_Group /c/Program\ Files
        fi
        cd ..
        echo Finished hd5 install 
