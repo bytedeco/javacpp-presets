2f20f2f83bd6911f0d1b018690c0e33c10ac18c8
vb216 <ubuntu@ip-172-31-31-125.eu-west-2.compute.internal>
2017-05-06 18:37:10 +0000

    detect if PR for windows builds

--- ci/build.cmd
+++ ci/build.cmd
@@ -7,8 +7,6 @@ mkdir %APPVEYOR_BUILD_FOLDER%\tmp
 set TMPDIR=%APPVEYOR_BUILD_FOLDER%\tmp
 mkdir %APPVEYOR_BUILD_FOLDER%\buildlogs
 
-echo %APPVEYOR_REPO_BRANCH%
-
 IF "%MSYS2_ARCH%"=="x86_64" (
    echo Callings vcvarsall for amd64
    call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
@@ -26,6 +24,8 @@ bash -lc "pacman -S --needed --noconfirm mingw-w64-x86_64-toolchain base-devel t
 
 bash -lc "/c/projects/javacpp-presets/ci/install-windows.sh %PROJ%"
 
+echo Building for %APPVEYOR_REPO_BRANCH%
+
 IF %COMPILER%==msys2 (
  @echo on
  echo Starting main build now.. 
@@ -35,7 +35,12 @@ IF %COMPILER%==msys2 (
  mvn install -Dmaven.test.skip=true -Djavacpp.platform=windows-%MSYS2_ARCH% -Dmaven.javadoc.skip=true
  cd ..
  cd javacpp-presets
- echo Install %PROJ%
- mvn install -Dmaven.test.skip=true -Dmaven.javadoc.skip=true -Djavacpp.platform=windows-%MSYS2_ARCH% --settings .\ci\settings.xml -pl %PROJ%
-
+ IF %APPVEYOR_REPO_BRANCH%=="master" (
+  echo Deploy snaphot for %PROJ%
+  mvn deploy -Dmaven.test.skip=true -Dmaven.javadoc.skip=true -Djavacpp.platform=windows-%MSYS2_ARCH% --settings .\ci\settings.xml -pl %PROJ%
+  )
+ ELSE (
+  echo Install %PROJ%
+  mvn install -Dmaven.test.skip=true -Dmaven.javadoc.skip=true -Djavacpp.platform=windows-%MSYS2_ARCH% -pl %PROJ%
+ )
 )
