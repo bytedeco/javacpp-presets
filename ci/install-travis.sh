1c3ec82d9674dc7ebbd07323cb0044298a3e0191
vb216 <ubuntu@ip-172-31-31-125.eu-west-2.compute.internal>
2017-05-24 13:19:20 +0000

    add mkl mac setup

--- ci/install-travis.sh
+++ ci/install-travis.sh
@@ -113,10 +113,11 @@ if [ "$TRAVIS_OS_NAME" == "osx" ]; then
         #don't put in download dir as will be cached and we can use direct url instead
         curl -L http://registrationcenter-download.intel.com/akdlm/irc_nas/tec/11563/m_mkl_2017.3.181.dmg -o $HOME/mkl.dmg
         echo "Mount mkl dmg"
-        hdiutil mount mkl.dmg
+        hdiutil mount $HOME/mkl.dmg
         sleep 10
-        sed -i -e 's/decline/accept/g' /Volumes/m_mkl_2017.3.181/m_mkl_2017.3.181.app/Contents/MacOS/silent.cfg
-        sudo /Volumes/m_mkl_2017.3.181/m_mkl_2017.3.181.app/Contents/MacOS/install.sh -s i/Volumes/m_mkl_2017.3.181/m_mkl_2017.3.181.app/Contents/MacOS/silent.cfg; export BREW_STATUS=$?
+        cp /Volumes/m_mkl_2017.3.181/m_mkl_2017.3.181.app/Contents/MacOS/silent.cfg $HOME/silent.cfg
+        sed -i -e 's/decline/accept/g' $HOME/silent.cfg
+        sudo /Volumes/m_mkl_2017.3.181/m_mkl_2017.3.181.app/Contents/MacOS/install.sh -s $HOME/silent.cfg; export BREW_STATUS=$?
         echo "mkl status $BREW_STATUS"
         if [ $BREW_STATUS -ne 0 ]; then
           echo "mkl Failed"
