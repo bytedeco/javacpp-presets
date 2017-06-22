da643db4923413680e939464fbb944431a04553e
vb216 <ubuntu@ip-172-31-31-125.eu-west-2.compute.internal>
2017-06-22 11:34:44 +0000

    update hdf5

--- ci/install-windows.sh
+++ ci/install-windows.sh
@@ -92,20 +92,20 @@ if [[ "$projectName" =~ "hdf5" ]]; then
              echo "Found hdf5-64 in cache and size seems OK"
           else
              /c/python27/python $APPVEYOR_BUILD_FOLDER/ci/gDownload.py 0B2xpvMUzviShTEtPYjU5SDhIZWM /c/Downloads/hdf5-64.zip 
-          fi
           unzip /c/Downloads/hdf5-64.zip 
-          cd hdf5
-          msiexec //i HDF5-1.10.1-win64.msi //quiet
+          fi
+          msiexec //i /c/Downloads/hdf5/HDF5-1.10.1-win64.msi //quiet
        elif [ "$MSYS2_ARCH" == "x86" ]; then
           echo 32bit copy for hdf5 
           if [[ $(find /c/Downloads/hdf5-32.zip -type f -size +1000000c 2>/dev/null) ]]; then
              echo "Found hdf5-32 in cache and size seems OK"
           else
              /c/python27/python $APPVEYOR_BUILD_FOLDER/ci/gDownload.py 0B2xpvMUzviShU1BzVTd1YzZGczg /c/Downloads/hdf5-32.zip 
-          fi
           unzip /c/Downloads/hdf5-32.zip 
-          cd hdf5
-          msiexec //i HDF5-1.10.1-win32.msi //quiet
+          fi
+          ls -ltr /c/Downloads
+          ls -ltr /c/Downloads/hdf5
+          msiexec //i /c/Downloads/hdf5/HDF5-1.10.1-win32.msi //quiet
        fi
        cd ..
        echo Finished hd5 install 
