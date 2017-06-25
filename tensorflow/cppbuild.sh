afab159fa2beac554f031adb8bf29c8fa37b8d97
Samuel Audet <samuel.audet@gmail.com>
2017-06-25 23:14:29 +0900

    * Upgrade presets for TensorFlow 1.2.0

--- tensorflow/cppbuild.sh
+++ tensorflow/cppbuild.sh
@@ -10,12 +10,15 @@ fi
 export PYTHON_BIN_PATH=$(which python)
 export USE_DEFAULT_PYTHON_LIB_PATH=1
 export CC_OPT_FLAGS=-O3
+export TF_NEED_MKL=0
+export TF_NEED_VERBS=0
 export TF_NEED_JEMALLOC=0
 export TF_NEED_CUDA=0
 export TF_NEED_GCP=0
 export TF_NEED_HDFS=0
 export TF_NEED_OPENCL=0
 export TF_ENABLE_XLA=0
+export TF_CUDA_CLANG=0
 export TF_CUDA_VERSION=8.0
 export TF_CUDNN_VERSION=6
 export GCC_HOST_COMPILER_PATH=$(which gcc)
@@ -23,7 +26,7 @@ export CUDA_TOOLKIT_PATH=/usr/local/cuda
 export CUDNN_INSTALL_PATH=$CUDA_TOOLKIT_PATH
 export TF_CUDA_COMPUTE_CAPABILITIES=3.0
 
-TENSORFLOW_VERSION=1.1.0
+TENSORFLOW_VERSION=1.2.0
 
 download https://github.com/tensorflow/tensorflow/archive/v$TENSORFLOW_VERSION.tar.gz tensorflow-$TENSORFLOW_VERSION.tar.gz
 
@@ -37,19 +40,21 @@ tar --totals -xzf ../tensorflow-$TENSORFLOW_VERSION.tar.gz
 cd tensorflow-$TENSORFLOW_VERSION
 
 case $PLATFORM in
+    # Clang is incapable of compiling TensorFlow for Android, while in $ANDROID_NDK/source.properties,
+    # the value of Pkg.Revision needs to start with "12" for Bazel to accept GCC
     android-arm)
         export CC="/usr/bin/gcc"
         export CXX="/usr/bin/g++"
         patch -Np1 < ../../../tensorflow-$TENSORFLOW_VERSION-android.patch
         sed -i "/    path=\"<PATH_TO_NDK>\",/c\    path=\"${ANDROID_NDK}\"," ./WORKSPACE
-        export BUILDFLAGS="--crosstool_top=//external:android/crosstool --cpu=armeabi-v7a --host_crosstool_top=@bazel_tools//tools/cpp:toolchain"
+        export BUILDFLAGS="--android_compiler=gcc-4.9 --crosstool_top=//external:android/crosstool --cpu=armeabi-v7a --host_crosstool_top=@bazel_tools//tools/cpp:toolchain"
         ;;
     android-x86)
         export CC="/usr/bin/gcc"
         export CXX="/usr/bin/g++"
         patch -Np1 < ../../../tensorflow-$TENSORFLOW_VERSION-android.patch
         sed -i "/    path=\"<PATH_TO_NDK>\",/c\    path=\"${ANDROID_NDK}\"," ./WORKSPACE
-        export BUILDFLAGS="--crosstool_top=//external:android/crosstool --cpu=x86 --host_crosstool_top=@bazel_tools//tools/cpp:toolchain"
+        export BUILDFLAGS="--android_compiler=gcc-4.9 --crosstool_top=//external:android/crosstool --cpu=x86 --host_crosstool_top=@bazel_tools//tools/cpp:toolchain"
         ;;
     linux-x86)
         export CC="/usr/bin/gcc"
@@ -67,7 +72,7 @@ case $PLATFORM in
         ;;
     macosx-*)
         export TF_NEED_CUDA=1
-        export BUILDFLAGS="--config=cuda --linkopt=-install_name --linkopt=@rpath/libtensorflow.so"
+        export BUILDFLAGS="--config=cuda --linkopt=-install_name --linkopt=@rpath/libtensorflow_cc.so"
         patch -Np1 < ../../../tensorflow-$TENSORFLOW_VERSION-nocuda.patch
         ;;
     *)
@@ -78,12 +83,5 @@ esac
 
 ./configure
 bazel build -c opt //tensorflow:libtensorflow_cc.so $BUILDFLAGS --spawn_strategy=standalone --genrule_strategy=standalone --output_filter=DONT_MATCH_ANYTHING --verbose_failures
-
-case $PLATFORM in
-    macosx-*)
-        chmod +w bazel-bin/tensorflow/libtensorflow_cc.so
-        install_name_tool -id @rpath/libtensorflow_cc.so bazel-bin/tensorflow/libtensorflow_cc.so
-        ;;
-esac
 
 cd ../..
