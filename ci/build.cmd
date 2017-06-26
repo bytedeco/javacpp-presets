23797c9d98df06ae8fafe3d7b0f6856adcfeb74d
vb216 <ubuntu@ip-172-31-31-125.eu-west-2.compute.internal>
2017-06-26 13:34:15 +0000

    test ffmpeg openssl fix

--- ci/build.cmd
+++ ci/build.cmd
@@ -7,6 +7,8 @@ mkdir %APPVEYOR_BUILD_FOLDER%\tmp
 set TMPDIR=%APPVEYOR_BUILD_FOLDER%\tmp
 mkdir %APPVEYOR_BUILD_FOLDER%\buildlogs
 
+echo "test" | pod2man
+
 IF "%MSYS2_ARCH%"=="x86_64" (
    echo Callings vcvarsall for amd64
    call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
@@ -16,7 +18,8 @@ IF "%MSYS2_ARCH%"=="x86" (
    call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
 )
 
-SET "PATH=C:\%MSYS2_DIR%\%MSYSTEM%\bin;C:\%MSYS2_DIR%\usr\bin;c:\perl\bin;%PATH%"
+SET "PATH=c:\perl\bin;C:\%MSYS2_DIR%\%MSYSTEM%\bin;C:\%MSYS2_DIR%\usr\bin;%PATH%"
+echo "test" | pod2man
 bash -lc "pacman -S --needed --noconfirm pacman-mirrors"
 bash -lc "pacman -Sy --noconfirm pacman"
 bash -lc "pacman -Syu --noconfirm"
@@ -25,6 +28,7 @@ bash -lc "pacman -S --needed --noconfirm base-devel tar nasm yasm pkg-config unz
 bash -lc "pacman -S --needed --noconfirm mingw-w64-x86_64-libtool mingw-w64-x86_64-cmake mingw-w64-x86_64-gcc-fortran mingw-w64-i686-gcc-fortran"
 
 bash -lc "/c/projects/javacpp-presets/ci/install-windows.sh %PROJ%"
+echo "test" | pod2man
 
 echo Building for "%APPVEYOR_REPO_BRANCH%"
 echo PR Number "%APPVEYOR_PULL_REQUEST_NUMBER%"
