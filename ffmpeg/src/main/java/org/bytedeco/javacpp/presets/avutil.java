a647edd917609f3f2eefb6cf54f43e725ca9ad58
Samuel Audet <samuel.audet@gmail.com>
2017-09-01 21:09:15 +0900

    Restore formatting

--- ffmpeg/src/main/java/org/bytedeco/javacpp/presets/avutil.java
+++ ffmpeg/src/main/java/org/bytedeco/javacpp/presets/avutil.java
@@ -29,6 +29,7 @@ import org.bytedeco.javacpp.tools.InfoMap;
 import org.bytedeco.javacpp.tools.InfoMapper;
 
 /**
+ *
  * @author Samuel Audet
  */
 @Properties(target="org.bytedeco.javacpp.avutil", value={
@@ -43,71 +44,71 @@ import org.bytedeco.javacpp.tools.InfoMapper;
 public class avutil implements InfoMapper {
     public void map(InfoMap infoMap) {
         infoMap.put(new Info("AV_NOPTS_VALUE").cppTypes("int64_t").translate(false))
-                .put(new Info("NAN", "INFINITY").cppTypes("double"))
-                .put(new Info("AV_TIME_BASE_Q", "PixelFormat", "CodecID", "AVCOL_SPC_YCGCO", "AVCOL_SPC_YCOCG").cppTypes())
-                .put(new Info("av_const").annotations("@Const"))
-                .put(new Info("FF_CONST_AVUTIL55").annotations())
-                .put(new Info("av_malloc_attrib", "av_alloc_size", "av_always_inline", "av_warn_unused_result").cppTypes().annotations())
-                .put(new Info("attribute_deprecated").annotations("@Deprecated"))
-                .put(new Info("AVPanScan", "AVCodecContext").cast().pointerTypes("Pointer"))
-                .put(new Info("FF_API_VAAPI").define())
-                .put(new Info("AV_PIX_FMT_ABI_GIT_MASTER", "AV_HAVE_INCOMPATIBLE_LIBAV_ABI", "!FF_API_XVMC",
-                        "FF_API_GET_BITS_PER_SAMPLE_FMT", "FF_API_FIND_OPT").define(false))
-                .put(new Info("AV_PIX_FMT_Y400A", "ff_check_pixfmt_descriptors").skip())
-                .put(new Info("AV_CH_FRONT_LEFT",
-                        "AV_CH_FRONT_RIGHT",
-                        "AV_CH_FRONT_CENTER",
-                        "AV_CH_LOW_FREQUENCY",
-                        "AV_CH_BACK_LEFT",
-                        "AV_CH_BACK_RIGHT",
-                        "AV_CH_FRONT_LEFT_OF_CENTER",
-                        "AV_CH_FRONT_RIGHT_OF_CENTER",
-                        "AV_CH_BACK_CENTER",
-                        "AV_CH_SIDE_LEFT",
-                        "AV_CH_SIDE_RIGHT",
-                        "AV_CH_TOP_CENTER",
-                        "AV_CH_TOP_FRONT_LEFT",
-                        "AV_CH_TOP_FRONT_CENTER",
-                        "AV_CH_TOP_FRONT_RIGHT",
-                        "AV_CH_TOP_BACK_LEFT",
-                        "AV_CH_TOP_BACK_CENTER",
-                        "AV_CH_TOP_BACK_RIGHT",
-                        "AV_CH_STEREO_LEFT",
-                        "AV_CH_STEREO_RIGHT",
-                        "AV_CH_WIDE_LEFT",
-                        "AV_CH_WIDE_RIGHT",
-                        "AV_CH_SURROUND_DIRECT_LEFT",
-                        "AV_CH_SURROUND_DIRECT_RIGHT",
-                        "AV_CH_LOW_FREQUENCY_2",
-                        "AV_CH_LAYOUT_NATIVE",
-                        "AV_CH_LAYOUT_MONO",
-                        "AV_CH_LAYOUT_STEREO",
-                        "AV_CH_LAYOUT_2POINT1",
-                        "AV_CH_LAYOUT_2_1",
-                        "AV_CH_LAYOUT_SURROUND",
-                        "AV_CH_LAYOUT_3POINT1",
-                        "AV_CH_LAYOUT_4POINT0",
-                        "AV_CH_LAYOUT_4POINT1",
-                        "AV_CH_LAYOUT_2_2",
-                        "AV_CH_LAYOUT_QUAD",
-                        "AV_CH_LAYOUT_5POINT0",
-                        "AV_CH_LAYOUT_5POINT1",
-                        "AV_CH_LAYOUT_5POINT0_BACK",
-                        "AV_CH_LAYOUT_5POINT1_BACK",
-                        "AV_CH_LAYOUT_6POINT0",
-                        "AV_CH_LAYOUT_6POINT0_FRONT",
-                        "AV_CH_LAYOUT_HEXAGONAL",
-                        "AV_CH_LAYOUT_6POINT1",
-                        "AV_CH_LAYOUT_6POINT1_BACK",
-                        "AV_CH_LAYOUT_6POINT1_FRONT",
-                        "AV_CH_LAYOUT_7POINT0",
-                        "AV_CH_LAYOUT_7POINT0_FRONT",
-                        "AV_CH_LAYOUT_7POINT1",
-                        "AV_CH_LAYOUT_7POINT1_WIDE",
-                        "AV_CH_LAYOUT_7POINT1_WIDE_BACK",
-                        "AV_CH_LAYOUT_OCTAGONAL",
-                        "AV_CH_LAYOUT_HEXADECAGONAL",
-                        "AV_CH_LAYOUT_STEREO_DOWNMIX").translate().cppTypes("long"))
-                .put(new Info("int (*)(void*, void*, int)").pointerTypes("Int_func_Pointer_Pointer_int"));
+               .put(new Info("NAN", "INFINITY").cppTypes("double"))
+               .put(new Info("AV_TIME_BASE_Q", "PixelFormat", "CodecID", "AVCOL_SPC_YCGCO", "AVCOL_SPC_YCOCG").cppTypes())
+               .put(new Info("av_const").annotations("@Const"))
+               .put(new Info("FF_CONST_AVUTIL55").annotations())
+               .put(new Info("av_malloc_attrib", "av_alloc_size", "av_always_inline", "av_warn_unused_result").cppTypes().annotations())
+               .put(new Info("attribute_deprecated").annotations("@Deprecated"))
+               .put(new Info("AVPanScan", "AVCodecContext").cast().pointerTypes("Pointer"))
+               .put(new Info("FF_API_VAAPI").define())
+               .put(new Info("AV_PIX_FMT_ABI_GIT_MASTER", "AV_HAVE_INCOMPATIBLE_LIBAV_ABI", "!FF_API_XVMC",
+                             "FF_API_GET_BITS_PER_SAMPLE_FMT", "FF_API_FIND_OPT").define(false))
+               .put(new Info("AV_PIX_FMT_Y400A", "ff_check_pixfmt_descriptors").skip())
+               .put(new Info("AV_CH_FRONT_LEFT",
+                             "AV_CH_FRONT_RIGHT",
+                             "AV_CH_FRONT_CENTER",
+                             "AV_CH_LOW_FREQUENCY",
+                             "AV_CH_BACK_LEFT",
+                             "AV_CH_BACK_RIGHT",
+                             "AV_CH_FRONT_LEFT_OF_CENTER",
+                             "AV_CH_FRONT_RIGHT_OF_CENTER",
+                             "AV_CH_BACK_CENTER",
+                             "AV_CH_SIDE_LEFT",
+                             "AV_CH_SIDE_RIGHT",
+                             "AV_CH_TOP_CENTER",
+                             "AV_CH_TOP_FRONT_LEFT",
+                             "AV_CH_TOP_FRONT_CENTER",
+                             "AV_CH_TOP_FRONT_RIGHT",
+                             "AV_CH_TOP_BACK_LEFT",
+                             "AV_CH_TOP_BACK_CENTER",
+                             "AV_CH_TOP_BACK_RIGHT",
+                             "AV_CH_STEREO_LEFT",
+                             "AV_CH_STEREO_RIGHT",
+                             "AV_CH_WIDE_LEFT",
+                             "AV_CH_WIDE_RIGHT",
+                             "AV_CH_SURROUND_DIRECT_LEFT",
+                             "AV_CH_SURROUND_DIRECT_RIGHT",
+                             "AV_CH_LOW_FREQUENCY_2",
+                             "AV_CH_LAYOUT_NATIVE",
+                             "AV_CH_LAYOUT_MONO",
+                             "AV_CH_LAYOUT_STEREO",
+                             "AV_CH_LAYOUT_2POINT1",
+                             "AV_CH_LAYOUT_2_1",
+                             "AV_CH_LAYOUT_SURROUND",
+                             "AV_CH_LAYOUT_3POINT1",
+                             "AV_CH_LAYOUT_4POINT0",
+                             "AV_CH_LAYOUT_4POINT1",
+                             "AV_CH_LAYOUT_2_2",
+                             "AV_CH_LAYOUT_QUAD",
+                             "AV_CH_LAYOUT_5POINT0",
+                             "AV_CH_LAYOUT_5POINT1",
+                             "AV_CH_LAYOUT_5POINT0_BACK",
+                             "AV_CH_LAYOUT_5POINT1_BACK",
+                             "AV_CH_LAYOUT_6POINT0",
+                             "AV_CH_LAYOUT_6POINT0_FRONT",
+                             "AV_CH_LAYOUT_HEXAGONAL",
+                             "AV_CH_LAYOUT_6POINT1",
+                             "AV_CH_LAYOUT_6POINT1_BACK",
+                             "AV_CH_LAYOUT_6POINT1_FRONT",
+                             "AV_CH_LAYOUT_7POINT0",
+                             "AV_CH_LAYOUT_7POINT0_FRONT",
+                             "AV_CH_LAYOUT_7POINT1",
+                             "AV_CH_LAYOUT_7POINT1_WIDE",
+                             "AV_CH_LAYOUT_7POINT1_WIDE_BACK",
+                             "AV_CH_LAYOUT_OCTAGONAL",
+                             "AV_CH_LAYOUT_HEXADECAGONAL",
+                             "AV_CH_LAYOUT_STEREO_DOWNMIX").translate().cppTypes("long"))
+               .put(new Info("int (*)(void*, void*, int)").pointerTypes("Int_func_Pointer_Pointer_int"));
     }
 }
