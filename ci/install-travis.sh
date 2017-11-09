8fcdd9c8f9e199f015353c59a556a4dbe7181ecc
Samuel Audet <samuel.audet@gmail.com>
2017-11-10 00:37:42 +0900

    Make sure pip is installed on Travis CI

--- ci/install-travis.sh
+++ ci/install-travis.sh
@@ -6,6 +6,7 @@ mkdir ./buildlogs
 mkdir $TRAVIS_BUILD_DIR/downloads
 ls -ltr $HOME/downloads
 ls -ltr $HOME/.m2
+sudo easy_install pip
 pip install requests
 export PYTHON_BIN_PATH=$(which python) # For tensorflow
 touch $HOME/vars.list
