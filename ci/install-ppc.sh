94f071c7d8a7e5f619a9c0171a3da11211dece66
vb216 <v.f.baines@gmail.com>
2017-12-22 16:13:22 +0000

    more fixes

--- ci/install-ppc.sh
+++ ci/install-ppc.sh
@@ -36,10 +36,10 @@ if [[ "$PROJ" =~ cuda ]]; then
    tar xvf data.tar.xz
    mkdir $HOME/downloads/cudaFS
    cd var; find . -name *.deb | while read line; do ar vx $line; tar --totals -xf data.tar.xz -C $HOME/downloads/cudaFS; done
-   docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -xec "mv $HOME/downloads/cudaFS/usr/local/cuda-9.1 /usr/local/cuda"  
-   #docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -xec "ln -s /usr/local/cuda-9.0 /usr/local/cuda"  
-   docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -xec "ln -s /usr/local/cuda/lib64/stubs/libcuda.so /usr/local/cuda/lib64/libcuda.so"  
-   docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -xec "tar --totals -xzf cudnn-9.1-linux-ppc64le-v7.tgz -C /usr/local/"
+   docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -xec "cd /usr;cp -R $HOME/downloads/cudaFS/usr/*"  
+   docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -xec "ln -s /usr/local/cuda-9.1 /usr/local/cuda"  
+   #docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -xec "ln -s /usr/local/cuda/lib64/stubs/libcuda.so /usr/local/cuda/lib64/libcuda.so"  
+   docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -xec "tar --totals -xzf $HOME/downloads/cudnn-9.1-linux-ppc64le-v7.tgz -C /usr/local/"
 fi
 
 echo "Running install for $PROJ"
