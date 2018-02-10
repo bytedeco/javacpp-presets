fa7aaf7cae74d7dc7a790c5d6a516e3e9933ce1e
Samuel Audet <samuel.audet@gmail.com>
2018-02-10 23:24:51 +0900

    Fix MSYS2 on AppVeyor

--- ci/build.cmd
+++ ci/build.cmd
@@ -22,8 +22,8 @@ IF "%OS%"=="windows-x86" (
 )
 echo on
 
-C:\msys64\usr\bin\bash -lc "pacman -Syu --noconfirm"
-C:\msys64\usr\bin\bash -lc "pacman -Su --noconfirm"
+rem C:\msys64\usr\bin\bash -lc "pacman -Syu --noconfirm"
+rem C:\msys64\usr\bin\bash -lc "pacman -Su --noconfirm"
 C:\msys64\usr\bin\bash -lc "pacman -S --needed --noconfirm base-devel git tar nasm yasm pkg-config unzip autoconf automake libtool make patch"
 C:\msys64\usr\bin\bash -lc "pacman -S --needed --noconfirm mingw-w64-x86_64-toolchain mingw-w64-x86_64-libtool mingw-w64-x86_64-cmake mingw-w64-x86_64-gcc mingw-w64-i686-gcc mingw-w64-x86_64-gcc-fortran mingw-w64-i686-gcc-fortran mingw-w64-x86_64-libwinpthread-git mingw-w64-i686-libwinpthread-git mingw-w64-x86_64-SDL mingw-w64-i686-SDL"
 
