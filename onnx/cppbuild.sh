c3c2a61db865522e1fa9945089bfefe19aa98976
Samuel Audet <samuel.audet@gmail.com>
2018-04-30 09:48:35 +0900

    Remove unneeded installation of Miniconda

--- onnx/cppbuild.sh
+++ onnx/cppbuild.sh
@@ -14,12 +14,6 @@ else
 
     export PROTO=2.6.1
     export INSTALL_PATH=`pwd`/$PLATFORM
-    wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh -O ~/miniconda.sh
-    bash ~/miniconda.sh -f -b -p $INSTALL_PATH/miniconda 
-    export PATH="$INSTALL_PATH/miniconda/bin:$PATH"
-    conda install -y -c conda-forge protobuf=2.6.1 numpy setuptools
-    export CFLAGS="-I$INSTALL_PATH/miniconda/include"
-    export CONDA_PREFIX="$INSTALL_PATH/miniconda"
     wget https://github.com/onnx/onnx/archive/v1.0.1.tar.gz
 
     mkdir -p $PLATFORM
@@ -28,16 +22,16 @@ else
     mv v$PROTO.tar.gz protobuf-$PROTO.tar.gz
     tar -xvf protobuf-$PROTO.tar.gz
 
-    export LIBRARY_PATH=`pwd`/lib
-    export PATH=$PATH:`pwd`/bin
-    export CFLAGS="-I`pwd`/include"
-    export CXXFLAGS="-I`pwd`/include"
+    export LIBRARY_PATH=$INSTALL_PATH/lib
+    export PATH=$PATH:$INSTALL_PATH/bin
+    export CFLAGS="-I$INSTALL_PATH/include"
+    export CXXFLAGS="-I$INSTALL_PATH/include"
     cd protobuf-$PROTO
 
     patch ./autogen.sh ../../../autogen.sh.patch
     ./autogen.sh
     ./configure "--prefix=$INSTALL_PATH" CFLAGS=-fPIC CXXFLAGS=-fPIC
-    make -j `nproc`
+    make -j $MAKEJ
     make install
 
     cd ..
@@ -53,7 +47,7 @@ else
     #export ONNX_ML=1
     export BASEDIR=build/temp.linux-x86_64-2.7/`pwd`/onnx/
 
-    python setup.py install --single-version-externally-managed --record=record.txt
+    python setup.py build
     g++ -v -std=c++11 -shared -Wl,-soname,libonnx.so -fPIC -o libonnx.so $BASEDIR/onnx-operators.pb.o $BASEDIR/onnx.pb.o $BASEDIR/checker.o $BASEDIR/defs/schema.o $BASEDIR/defs/tensor/old.o $BASEDIR/defs/tensor/defs.o $BASEDIR/defs/generator/defs.o $BASEDIR/defs/math/defs.o $BASEDIR/defs/data_type_utils.o $BASEDIR/defs/traditionalml/defs.o $BASEDIR/defs/experiments/defs.o $BASEDIR/defs/nn/defs.o $BASEDIR/defs/nn/old.o $BASEDIR/defs/reduction/defs.o $BASEDIR/defs/logical/defs.o $BASEDIR/defs/rnn/defs.o -pthread
     cd ..
 
