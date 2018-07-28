e4d29e677ef9349f212bfabde5dbfcf6810d4daf
Samuel Audet <samuel.audet@gmail.com>
2018-07-28 15:17:32 +0900

    Reenable Windows builds for TensorFlow on AppVeyor CI
    Apparently, we should not call "exit /b" to exit from a batch file,
    but just "exit", or AppVeyor may incorrectly report an exit code of 259

--- ci/build.cmd
+++ ci/build.cmd
@@ -49,9 +49,9 @@ IF "%PARTIAL_CPPBUILD%"=="1" (
    C:\msys64\usr\bin\bash -c "zip -r %PROJ%-cppbuild.zip %PROJ%/cppbuild"
    IF ERRORLEVEL 1 (
      echo Quitting with error  
-     exit /b 1
+     exit 1
    )
-   exit /b 0
+   exit 0
 )
 
 IF "%APPVEYOR_PULL_REQUEST_NUMBER%"=="" (
@@ -59,7 +59,7 @@ IF "%APPVEYOR_PULL_REQUEST_NUMBER%"=="" (
    call mvn clean deploy -B -U --settings .\ci\settings.xml -Dmaven.test.skip=true %MAVEN_RELEASE% -Djavacpp.platform=%OS% -Djavacpp.platform.extension=%EXT% -pl .,%PROJ%
    IF ERRORLEVEL 1 (
      echo Quitting with error  
-     exit /b 1
+     exit 1
    )
    FOR %%a in ("%PROJ:,=" "%") do (
     echo Deploy platform %%a 
@@ -67,7 +67,7 @@ IF "%APPVEYOR_PULL_REQUEST_NUMBER%"=="" (
     call mvn clean deploy -B -U --settings ..\ci\settings.xml -f platform -Dmaven.test.skip=true %MAVEN_RELEASE% -Djavacpp.platform=%OS% -Djavacpp.platform.extension=%EXT%
     IF ERRORLEVEL 1 (
       echo Quitting with error  
-      exit /b 1
+      exit 1
     )
 
     cd ..
@@ -77,8 +77,9 @@ IF "%APPVEYOR_PULL_REQUEST_NUMBER%"=="" (
    call mvn clean install -B -U --settings .\ci\settings.xml -Dmaven.test.skip=true %MAVEN_RELEASE% -Djavacpp.platform=%OS% -Djavacpp.platform.extension=%EXT% -pl .,%PROJ%
    IF ERRORLEVEL 1 (
       echo Quitting with error  
-      exit /b 1 
+      exit 1
    )
 
 )
+exit 0
 
