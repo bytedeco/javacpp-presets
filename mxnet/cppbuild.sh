99b82ace7c5e6f9882e1fa076ab294f6a1656a7e
Nico Hezel <Neiko2002@users.noreply.github.com>
2018-10-02 15:03:25 +0200

    * Add support for Windows to presets for MXNet and enable MKL-DNN (pull #309)

--- mxnet/cppbuild.sh
+++ mxnet/cppbuild.sh
@@ -14,38 +14,13 @@ export CUDA_ARCH=-arch=sm_30
 export USE_CUDA=0
 export USE_CUDNN=0
 export USE_CUDA_PATH=
+export USE_MKLDNN=1
 if [[ "$EXTENSION" == *gpu ]]; then
     export ADD_CFLAGS="$ADD_CFLAGS -DMXNET_USE_CUDA=1"
     export USE_CUDA=1
     export USE_CUDNN=1
     export USE_CUDA_PATH="/usr/local/cuda"
 fi
-
-case $PLATFORM in
-    linux-x86)
-        export CC="gcc -m32"
-        export CXX="g++ -m32"
-        export BLAS="openblas"
-        ;;
-    linux-x86_64)
-        export CC="gcc -m64"
-        export CXX="g++ -m64"
-        if which g++-6 &> /dev/null; then
-            export CC="gcc-6 -m64"
-            export CXX="g++-6 -m64"
-        fi
-        export BLAS="openblas"
-        ;;
-    macosx-*)
-        export CC="clang"
-        export CXX="clang++"
-        export BLAS="openblas"
-        ;;
-    *)
-        echo "Error: Platform \"$PLATFORM\" is not supported"
-        return 0
-        ;;
-esac
 
 MXNET_VERSION=1.3.0
 download http://apache.org/dist/incubator/mxnet/$MXNET_VERSION/apache-mxnet-src-$MXNET_VERSION-incubating.tar.gz apache-mxnet-src-$MXNET_VERSION-incubating.tar.gz
@@ -78,15 +53,70 @@ cd apache-mxnet-src-$MXNET_VERSION-incubating
 sedinplace 's/kCPU/Context::kCPU/g' src/operator/tensor/elemwise_binary_scalar_op_basic.cc
 sedinplace 's:../../src/operator/tensor/:./:g' src/operator/tensor/cast_storage-inl.h
 
+case $PLATFORM in
+    linux-x86)
+        export CC="gcc -m32"
+        export CXX="g++ -m32"
+        export BLAS="openblas"
+        export USE_MKLDNN=0
+        ;;
+    linux-x86_64)
+        export CC="gcc -m64"
+        export CXX="g++ -m64"
+        if which g++-6 &> /dev/null; then
+            export CC="gcc-6 -m64"
+            export CXX="g++-6 -m64"
+        fi
+        export BLAS="openblas"
+        ;;
+    macosx-*)
+        export CC="clang"
+        export CXX="clang++"
+        export BLAS="openblas"
+        ;;
+    windows-x86_64)
+        # copy include files
+        mkdir -p ../include
+        cp -r include/mxnet 3rdparty/dmlc-core/include/dmlc 3rdparty/mshadow/mshadow ../include
+
+        # configure the build
+        mkdir -p ../build
+        cd ../build
+        USE_X="-DCUDA_ARCH_LIST=3.0+PTX -DUSE_CUDA=$USE_CUDA -DUSE_CUDNN=$USE_CUDNN -DUSE_OPENCV=ON -DUSE_MKLDNN=$USE_MKLDNN"
+        OPENCV="-DOpenCV_DIR=$OPENCV_PATH/ -DOpenCV_CONFIG_PATH=$OPENCV_PATH/"
+        OPENBLAS="-DOpenBLAS_INCLUDE_DIR=$OPENBLAS_PATH/include/ -DOpenBLAS_LIB=$OPENBLAS_PATH/lib/openblas.lib"
+        "$CMAKE" -G "Visual Studio 14 2015 Win64" $USE_X $OPENCV $OPENBLAS ../apache-mxnet-src-$MXNET_VERSION-incubating
+
+        # build the project
+        MSBuild.exe ALL_BUILD.vcxproj //p:Configuration=Release //maxcpucount:$MAKEJ
+
+        # copy binary files
+        mkdir -p ../bin
+        cp Release/*.dll 3rdparty/mkldnn/src/Release/*.dll ../bin
+
+        # copy library files
+        mkdir -p ../lib
+        cp Release/libmxnet.lib ../lib/mxnet.lib
+
+        # finish
+        cd ../..
+        return 0
+        ;;
+    *)
+        echo "Error: Platform \"$PLATFORM\" is not supported"
+        return 0
+        ;;
+esac
+
 export C_INCLUDE_PATH="$OPENBLAS_PATH/include/:$OPENCV_PATH/include/"
 export CPLUS_INCLUDE_PATH="$C_INCLUDE_PATH"
 export LIBRARY_PATH="$OPENBLAS_PATH/:$OPENBLAS_PATH/lib/:$OPENCV_PATH/:$OPENCV_PATH/lib/"
 
 sed -i="" 's/$(shell pkg-config --cflags opencv)//' Makefile
 sed -i="" 's/$(shell pkg-config --libs opencv)/-lopencv_highgui -lopencv_imgcodecs -lopencv_imgproc -lopencv_core/' Makefile
-make -j $MAKEJ CC="$CC" CXX="$CXX" USE_BLAS="$BLAS" USE_OPENMP="$USE_OPENMP" CUDA_ARCH="$CUDA_ARCH" USE_CUDA="$USE_CUDA" USE_CUDNN="$USE_CUDNN" USE_CUDA_PATH="$USE_CUDA_PATH" USE_F16C=0 ADD_CFLAGS="$ADD_CFLAGS" ADD_LDFLAGS="$ADD_LDFLAGS" lib/libmxnet.a lib/libmxnet.so
-cp -a include lib 3rdparty/dmlc-core/include ..
-cp -a 3rdparty/mshadow/mshadow ../include
+make -j $MAKEJ CC="$CC" CXX="$CXX" USE_BLAS="$BLAS" USE_OPENMP="$USE_OPENMP" CUDA_ARCH="$CUDA_ARCH" USE_CUDA="$USE_CUDA" USE_CUDNN="$USE_CUDNN" USE_CUDA_PATH="$USE_CUDA_PATH" USE_MKLDNN="$USE_MKLDNN" USE_F16C=0 ADD_CFLAGS="$ADD_CFLAGS" ADD_LDFLAGS="$ADD_LDFLAGS" lib/libmxnet.a lib/libmxnet.so
+cp -r include lib 3rdparty/dmlc-core/include ..
+cp -r 3rdparty/mshadow/mshadow ../include
 unset CC
 unset CXX
 
