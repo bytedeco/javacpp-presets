42fe0a401058b2a314d308480f296f869fd7ed5d
Jarek Sacha <jpsacha@gmail.com>
2018-12-18 22:23:54 -0500

    Change Spinnaker to build without docker.
    Need to also change logic that determines if maven build is done in docker.

--- ci/install-travis.sh
+++ ci/install-travis.sh
@@ -98,17 +98,6 @@ if [[ "$OS" == "linux-x86" ]] || [[ "$OS" == "linux-x86_64" ]] || [[ "$OS" =~ an
         docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -xec "cp -R $HOME/build/include/* /usr/include; cp -R $HOME/build/lib/* /usr/lib"
     fi
   fi
-  if [[ "$PROJ" =~ spinnaker ]]; then
-    if [ "$OS" == "linux-x86_64" ]; then
-        if [[ $(find $HOME/downloads/spinnaker_local_v.1.15.0.63.tar.gz -type f -size +1000000c 2>/dev/null) ]]; then
-          echo "Found spinnaker in cache and size seems ok"
-        else
-          echo "Downloading spinnaker as not found in cache or too small"
-          python $TRAVIS_BUILD_DIR/ci/gDownload.py 1IYtvqzpNHJgZK-TPztW_WDYuDEyo56D_ $HOME/downloads/spinnaker_local_v.1.15.0.63.tar.gz
-        fi
-        docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -xec "tar xvf $HOME/downloads/spinnaker_local_v.1.15.0.63.tar.gz -C /"
-    fi
-  fi
   if [[ "$PROJ" == "mkl" ]] && [[ "$OS" =~ linux ]]; then
          #don't put in download dir as will be cached and we can use direct url instead
          curl -L http://registrationcenter-download.intel.com/akdlm/irc_nas/tec/14895/l_mkl_2019.1.144.tgz -o $HOME/mkl.tgz
@@ -131,6 +120,26 @@ if [[ "$OS" == "linux-x86" ]] || [[ "$OS" == "linux-x86_64" ]] || [[ "$OS" =~ an
         python $TRAVIS_BUILD_DIR/ci/gDownload.py 1l9W2_vtYftijNave2OTitCepqyHrZu_- $HOME/downloads/tensorrt.tar.gz
         docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -xec "tar xvf $HOME/downloads/tensorrt.tar.gz -C /usr/local/; ln -sf /usr/local/TensorRT* /usr/local/tensorrt"
   fi
+fi
+
+if [[ "$PROJ" =~ spinnaker ]] && [[ "$OS" == "linux-x86_64" ]]; then
+    echo "Installing Spinnaker SDK"
+    SPIN_DOWNLOAD_XDIR="spinnaker-1.19.0.22-amd64"
+    SPIN_DOWNLOAD_NAME="spinnaker-1.19.0.22-amd64-pkg.tar.gz"
+    SPIN_DOWNLOAD_LINK="1Qe9mzN17h1z6oXNxmRg0-xkcnBTL8rTK"
+    SPIN_DEPENDENCIES="libavcodec57 libavformat57 libswscale4 libswresample2 libavutil55 libusb-1.0-0 libgtkmm-2.4-dev"
+    # Download Spinnaker SDK
+    if [[ $(find ${HOME}/downloads/${SPIN_DOWNLOAD_NAME} -type f -size +1000000c 2>/dev/null) ]]; then
+        echo "Found spinnaker in cache and size seems ok"
+    else
+        echo "Downloading spinnaker as not found in cache or too small"
+        python $TRAVIS_BUILD_DIR/ci/gDownload.py ${SPIN_DOWNLOAD_LINK} ${HOME}/downloads/${SPIN_DOWNLOAD_NAME}
+    fi
+    # Install Spinnaker packages
+    #  sudo apt-get -y install ${SPIN_DEPENDENCIES}
+    tar xzvf ${HOME}/downloads/${SPIN_DOWNLOAD_NAME} -C ${TRAVIS_BUILD_DIR}/build/
+    sudo dpkg -i ${TRAVIS_BUILD_DIR}/build/${SPIN_DOWNLOAD_XDIR}/*.deb
+    rm -rf ${TRAVIS_BUILD_DIR}/build/${SPIN_DOWNLOAD_XDIR}
 fi
 
 if [ "$OS" == "linux-armhf" ]; then
@@ -274,9 +283,12 @@ fi
 
 
 echo "Running install for $PROJ"
-if  [[ "$OS" == "linux-x86" ]] || [[ "$OS" == "linux-x86_64" ]] || [[ "$OS" =~ android ]]; then
-   DOCKER_CONTAINER_ID=$(docker ps | grep centos | awk '{print $1}')
-   echo "container id is $DOCKER_CONTAINER_ID"
+# Assume that if docker ID was defined a docker build should be used
+echo "container id is $DOCKER_CONTAINER_ID"
+if [[ -n $DOCKER_CONTAINER_ID ]]; then
+#if  [[ "$OS" == "linux-x86" ]] || [[ "$OS" == "linux-x86_64" ]] || [[ "$OS" =~ android ]]; then
+#   DOCKER_CONTAINER_ID=$(docker ps | grep centos | awk '{print $1}')
+#   echo "container id is $DOCKER_CONTAINER_ID"
     if [ "$1" == "nodeploy" ]; then
        docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -xec "source scl_source enable $SCL_ENABLE || true; . $HOME/vars.list; cd $HOME/build/javacpp-presets; bash cppbuild.sh install $PROJ -platform=$OS -extension=$EXT"; export BUILD_STATUS=0
     elif [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
