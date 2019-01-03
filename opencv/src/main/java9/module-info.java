ea2c85ddbd38551e3961065fa83b4218a54e80c5
Hervé Guillemet <hg@apteryx.fr>
2019-01-03 15:39:23 +0100

    Added prefix org/bytedeco/javacpp prefix to jar exclude list Added exports in module info for org.opencv.* Changed compiler output directory for android moditect version changed to beta2

--- opencv/src/main/java9/module-info.java
+++ opencv/src/main/java9/module-info.java
@@ -1,5 +1,6 @@
 module org.bytedeco.javacpp.opencv {
     requires transitive org.bytedeco.javacpp;
+
     exports org.bytedeco.javacpp.opencv_aruco;
     exports org.bytedeco.javacpp.opencv_bgsegm;
     exports org.bytedeco.javacpp.opencv_bioinspired;
@@ -38,4 +39,33 @@ module org.bytedeco.javacpp.opencv {
     exports org.bytedeco.javacpp.opencv_xfeatures2d;
     exports org.bytedeco.javacpp.opencv_ximgproc;
     exports org.bytedeco.javacpp.opencv_xphoto;
+
+    exports org.opencv.android;
+    exports org.opencv.aruco;
+    exports org.opencv.bgsegm;
+    exports org.opencv.bioinspired;
+    exports org.opencv.calib3d;
+    exports org.opencv.core;
+    exports org.opencv.dnn;
+    exports org.opencv.face;
+    exports org.opencv.features2d;
+    exports org.opencv.highgui;
+    exports org.opencv.imgcodecs;
+    exports org.opencv.img_hash;
+    exports org.opencv.imgproc;
+    exports org.opencv.ml;
+    exports org.opencv.objdetect;
+    exports org.opencv.osgi;
+    exports org.opencv.phase_unwrapping;
+    exports org.opencv.photo;
+    exports org.opencv.plot;
+    exports org.opencv.structured_light;
+    exports org.opencv.text;
+    exports org.opencv.tracking;
+    exports org.opencv.utils;
+    exports org.opencv.video;
+    exports org.opencv.videoio;
+    exports org.opencv.xfeatures2d;
+    exports org.opencv.ximgproc;
+    exports org.opencv.xphoto;
 }
