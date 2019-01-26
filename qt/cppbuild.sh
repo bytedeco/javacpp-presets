1c38c6595c3dce469c8011ad257f10e42a3cce8f
Samuel Audet <samuel.audet@gmail.com>
2019-01-26 23:54:39 +0900

    Fix build for Qt on Mac

--- qt/cppbuild.sh
+++ qt/cppbuild.sh
@@ -18,7 +18,7 @@ tar --totals -xf ../qt-$QT_VERSION.tar.xz
 
 cd qt-everywhere-src-$QT_VERSION
 
-QT_OPTIONS="-prefix .. -opensource -confirm-license -nomake examples -nomake tests -nomake tools -skip qt3d -skip qtactiveqt -skip qtandroidextras -skip qtcanvas3d -skip qtcharts -skip qtconnectivity -skip qtdatavis3d -skip qtdeclarative -skip qtdoc -skip qtgamepad -skip qtgraphicaleffects -skip qtimageformats -skip qtlocation -skip qtmacextras -skip qtmultimedia -skip qtnetworkauth -skip qtpurchasing -skip qtquickcontrols -skip qtquickcontrols2 -skip qtremoteobjects -skip qtscript -skip qtscxml -skip qtsensors -skip qtserialbus -skip qtserialport -skip qtspeech -skip qtsvg -skip qttools -skip qttranslations -skip qtvirtualkeyboard -skip qtwayland -skip qtwebchannel -skip qtwebengine -skip qtwebglplugin -skip websockets -skip qtwebview -skip qtwinextras -skip qtx11extras -skip qtxmlpatterns -release -silent"
+QT_OPTIONS="-prefix .. -opensource -confirm-license -nomake examples -nomake tests -nomake tools -skip qt3d -skip qtactiveqt -skip qtandroidextras -skip qtcanvas3d -skip qtcharts -skip qtconnectivity -skip qtdatavis3d -skip qtdeclarative -skip qtdoc -skip qtgamepad -skip qtgraphicaleffects -skip qtimageformats -skip qtlocation -skip qtmacextras -skip qtmultimedia -skip qtnetworkauth -skip qtpurchasing -skip qtquickcontrols -skip qtquickcontrols2 -skip qtremoteobjects -skip qtscript -skip qtscxml -skip qtsensors -skip qtserialbus -skip qtserialport -skip qtspeech -skip qtsvg -skip qttools -skip qttranslations -skip qtvirtualkeyboard -skip qtwayland -skip qtwebchannel -skip qtwebengine -skip qtwebglplugin -skip websockets -skip qtwebview -skip qtwinextras -skip qtx11extras -skip qtxmlpatterns -no-framework -release -silent"
 
 case $PLATFORM in
     linux-x86_64)
@@ -30,9 +30,6 @@ case $PLATFORM in
         ./configure $QT_OPTIONS
         make -j $MAKEJ
         make install
-        ln -sf ../lib/QtCore.framework/Headers ../include/QtCore
-        ln -sf ../lib/QtGui.framework/Headers ../include/QtGui
-        ln -sf ../lib/QtWidgets.framework/Headers ../include/QtWidgets
         ;;
     *)
         echo "Error: Platform \"$PLATFORM\" is not supported"
