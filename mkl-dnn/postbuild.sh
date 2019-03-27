0b530360827c12ddd11679e3653865ab34ea26cc
Samuel Audet <samuel.audet@gmail.com>
2019-03-27 15:58:40 +0900

    Fix build for MKL-DNN on Mac

--- mkl-dnn/postbuild.sh
+++ mkl-dnn/postbuild.sh
@@ -2,7 +2,7 @@
 # Fix GCC library incorrectly linking for rpath on Mac
 for VER in 5 6 7 8; do
     for LIB in libmkldnn.0.dylib libjnimkldnn.dylib libjnimklml.dylib; do
-        LIBFILE=target/classes/org/bytedeco/javacpp/macosx-x86_64/$LIB
+        LIBFILE=target/native/org/bytedeco/mkldnn/macosx-x86_64/$LIB
         if [[ -f $LIBFILE ]]; then
             echo Fixing $LIBFILE
             install_name_tool -change /usr/local/lib/gcc/$VER/libgcc_s.1.dylib @rpath/libgcc_s.1.dylib $LIBFILE
