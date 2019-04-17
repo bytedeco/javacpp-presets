3fdf90071ff59d6c6b222223c12e934475a4737d
HGuillemet <hg@apteryx.fr>
2019-04-17 11:04:52 +0200

    Add missing exports to module-info.java files for all presets (pull #716)

--- spinnaker/src/main/java9/module-info.java
+++ spinnaker/src/main/java9/module-info.java
@@ -1,5 +1,6 @@
 module org.bytedeco.spinnaker {
   requires transitive org.bytedeco.javacpp;
   exports org.bytedeco.spinnaker.global;
+  exports org.bytedeco.spinnaker.presets;
   exports org.bytedeco.spinnaker.Spinnaker_C;
 }
