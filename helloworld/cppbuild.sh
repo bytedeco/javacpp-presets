163fb31f1344877ffdb0c82da8b170e96dfdbd28
Matteo Di Giovinazzo <matteodg@gmail.com>
2019-05-31 02:42:06 -0400

    Fixed include helloworld.h

--- helloworld/cppbuild.sh
+++ helloworld/cppbuild.sh
@@ -13,15 +13,16 @@ download https://github.com/matteodg/helloworld/archive/$HELLOWORLD_VERSION.zip 
 mkdir -p $PLATFORM
 cd $PLATFORM
 INSTALL_PATH=`pwd`
+
+#git clone https://github.com/matteodg/helloworld.git helloworld-$HELLOWORLD_VERSION
 echo "Decompressing archives..."
 unzip -o ../helloworld-$HELLOWORLD_VERSION.zip
+
 cd helloworld-$HELLOWORLD_VERSION
 
 case $PLATFORM in
     linux-x86|linux-x86_64|macosx-*|windows-x86|windows-x86_64)
-        aclocal
-        autoconf
-        automake --add-missing
+	./autogen.sh
         ./configure --prefix=$INSTALL_PATH
         make
         make install
--- helloworld/src/gen/java/org/bytedeco/helloworld/global/helloworld.java
+++ helloworld/src/gen/java/org/bytedeco/helloworld/global/helloworld.java
@@ -13,6 +13,29 @@ public class helloworld extends org.bytedeco.helloworld.presets.helloworld {
 
 // Parsed from helloworld.h
 
+/*
+ * Copyright 2019 Matteo Di Giovinazzo.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * File:   helloworld.h
+ * Author: Matteo Di Giovinazzo
+ *
+ * Created on May 9, 2019, 12:10 PM
+ */
+
 // #ifndef helloworld_h__
 // #define helloworld_h__
     public static native @Cast("bool") boolean getBool();
