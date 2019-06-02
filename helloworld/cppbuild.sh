6419e88095d9ed8834f0e07ce42f20d34ff4731f
Matteo Di Giovinazzo <digiovinazzo@streamsim.com>
2019-06-02 09:39:26 -0600

    Added m32 flag to compile 32bit exes

--- helloworld/cppbuild.sh
+++ helloworld/cppbuild.sh
@@ -21,7 +21,13 @@ unzip -o ../helloworld-$HELLOWORLD_VERSION.zip
 cd helloworld-$HELLOWORLD_VERSION
 
 case $PLATFORM in
-    linux-x86|linux-x86_64|macosx-*|windows-x86|windows-x86_64)
+    linux-x86|windows-x86)
+	./autogen.sh
+        ./configure "--prefix=$INSTALL_PATH" "CFLAGS=-m32" "CXXFLAGS=-m32" "LDFLAGS=-m32"
+        make
+        make install
+        ;;
+    linux-x86_64|macosx-*|windows-x86_64)
 	./autogen.sh
         ./configure --prefix=$INSTALL_PATH
         make
