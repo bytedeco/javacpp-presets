b02c8a0714570a768b0dc444f8bd35921592be25
Matteo Di Giovinazzo <482618+matteodg@users.noreply.github.com>
2019-06-21 07:43:05 -0600

    * Add presets for helloworld sample project (pull #725)

---
+++ helloworld/cppbuild.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+# This file is meant to be included by the parent cppbuild.sh script
+if [[ -z "$PLATFORM" ]]; then
+    pushd ..
+    bash cppbuild.sh "$@" helloworld
+    popd
+    exit
+fi
+
+HELLOWORLD_VERSION=1.0
+download https://github.com/bytedeco/helloworld/archive/$HELLOWORLD_VERSION.zip helloworld-$HELLOWORLD_VERSION.zip
+
+mkdir -p $PLATFORM
+cd $PLATFORM
+INSTALL_PATH=`pwd`
+
+#git clone https://github.com/bytedeco/helloworld.git helloworld-$HELLOWORLD_VERSION
+echo "Decompressing archives..."
+unzip -o ../helloworld-$HELLOWORLD_VERSION.zip
+
+cd helloworld-$HELLOWORLD_VERSION
+
+case $PLATFORM in
+    linux-x86|windows-x86)
+        ./autogen.sh
+        ./configure "--prefix=$INSTALL_PATH" "CFLAGS=-m32" "CXXFLAGS=-m32" "LDFLAGS=-m32"
+        make
+        make install
+        ;;
+    linux-x86_64|macosx-*|windows-x86_64)
+        ./autogen.sh
+        ./configure --prefix=$INSTALL_PATH
+        make
+        make install
+        ;;
+    *)
+        echo "Error: Platform \"$PLATFORM\" is not supported"
+        ;;
+esac
+
+cd ../..
---
+++ helloworld/src/main/java/org/bytedeco/helloworld/presets/helloworld.java
@@ -0,0 +1,28 @@
+package org.bytedeco.helloworld.presets;
+
+import org.bytedeco.javacpp.annotation.*;
+import org.bytedeco.javacpp.tools.*;
+
+@Properties(
+        target = "org.bytedeco.helloworld",
+        global = "org.bytedeco.helloworld.global.helloworld",
+        value = {
+            @Platform(
+                    value = {
+                        "linux-x86",
+                        "linux-x86_64",
+                        "macosx-x86_64",
+                        "windows-x86",
+                        "windows-x86_64"
+                    },
+                    include = "helloworld.h",
+                    link = "helloworld"
+            )
+        }
+)
+public class helloworld implements InfoMapper {
+
+    @Override
+    public void map(InfoMap infoMap) {
+    }
+}
