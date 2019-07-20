5ba4e7c5a5a7af3d6e3c01cf0ad7fd0a6c534f6d
Alex Merritt <lecaran@gmail.com>
2019-07-20 12:31:27 -0300

    Update MKL-DNN Preset to 1.0; Remove MKLML

--- mkl-dnn/src/gen/java/org/bytedeco/mkldnn/mkldnn_memory.java
+++ mkl-dnn/src/gen/java/org/bytedeco/mkldnn/mkldnn_memory.java
@@ -9,17 +9,12 @@ import org.bytedeco.javacpp.annotation.*;
 import static org.bytedeco.mkldnn.global.mkldnn.*;
 
 
-/** \}
- <p>
- *  \addtogroup c_api_primitive_descs Primitive descriptors
- *  \{
- <p>
- *  \struct mkldnn_primitive_desc
- *  \brief An opaque structure to describe a primitive descriptor. */
+/** \struct mkldnn_memory
+ *  An opaque structure to describe a memory. */
 @Opaque @Properties(inherit = org.bytedeco.mkldnn.presets.mkldnn.class)
-public class mkldnn_primitive_desc extends Pointer {
+public class mkldnn_memory extends Pointer {
     /** Empty constructor. Calls {@code super((Pointer)null)}. */
-    public mkldnn_primitive_desc() { super((Pointer)null); }
+    public mkldnn_memory() { super((Pointer)null); }
     /** Pointer cast constructor. Invokes {@link Pointer#Pointer(Pointer)}. */
-    public mkldnn_primitive_desc(Pointer p) { super(p); }
+    public mkldnn_memory(Pointer p) { super(p); }
 }
