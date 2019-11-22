1f4c79bf3f72791b94ef6edc3337a6c15fc45d73
Samuel Audet <samuel.audet@gmail.com>
2019-11-22 15:55:33 +0900

    * Bundle OpenSSL in the presets for CPython for consistency across platforms (issue #796)

--- ci/install-ppc.sh
+++ ci/install-ppc.sh
@@ -57,8 +57,8 @@ docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -xec 'echo "deb [arch=amd64] http
 docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -xec "sed -i 's/deb http/deb [arch=i386,amd64] http/g' /etc/apt/sources.list"
 docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -xec "apt-key adv --keyserver keyserver.ubuntu.com --recv-keys EB9B1D8886F44E2A"
 docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -xec "apt-get update"
-docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -xec "apt-get -y install python python2.7 python-minimal python2.7-minimal libgtk2.0-dev:ppc64el libasound2-dev:ppc64el libusb-dev:ppc64el libusb-1.0-0-dev:ppc64el libffi-dev:ppc64el libssl-dev:ppc64el zlib1g-dev:ppc64el libxcb1-dev:ppc64el"
-docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -xec "apt-get -y install pkg-config ccache gcc-powerpc64le-linux-gnu g++-powerpc64le-linux-gnu gfortran-powerpc64le-linux-gnu linux-libc-dev-ppc64el-cross binutils-multiarch openjdk-8-jdk-headless ant python python-dev swig git file wget unzip tar bzip2 patch autoconf-archive autogen automake make libtool perl nasm yasm curl cmake libffi-dev libssl-dev zlib1g-dev"
+docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -xec "apt-get -y install python python2.7 python-minimal python2.7-minimal libgtk2.0-dev:ppc64el libasound2-dev:ppc64el libusb-dev:ppc64el libusb-1.0-0-dev:ppc64el libffi-dev:ppc64el zlib1g-dev:ppc64el libxcb1-dev:ppc64el"
+docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -xec "apt-get -y install pkg-config ccache gcc-powerpc64le-linux-gnu g++-powerpc64le-linux-gnu gfortran-powerpc64le-linux-gnu linux-libc-dev-ppc64el-cross binutils-multiarch openjdk-8-jdk-headless ant python python-dev swig git file wget unzip tar bzip2 patch autoconf-archive autogen automake make libtool perl nasm yasm curl cmake libffi-dev zlib1g-dev"
 
 if [[ "$PROJ" =~ cuda ]]; then
    echo "Setting up for cuda build"
--- cpython/cppbuild.sh
+++ cpython/cppbuild.sh
@@ -7,7 +7,9 @@ if [[ -z "$PLATFORM" ]]; then
     exit
 fi
 
+OPENSSL=openssl-1.1.1d
 CPYTHON_VERSION=3.7.5
+download https://www.openssl.org/source/$OPENSSL.tar.gz $OPENSSL.tar.gz
 download https://www.python.org/ftp/python/$CPYTHON_VERSION/Python-$CPYTHON_VERSION.tgz Python-$CPYTHON_VERSION.tgz
 
 mkdir -p $PLATFORM
@@ -15,53 +17,120 @@ cd $PLATFORM
 INSTALL_PATH=`pwd`
 
 echo "Decompressing archives..."
+tar --totals -xzf ../$OPENSSL.tar.gz
 tar --totals -xzf ../Python-$CPYTHON_VERSION.tgz
 cd Python-$CPYTHON_VERSION
 patch -Np1 --binary < ../../../cpython-windows.patch
 
 case $PLATFORM in
     linux-armhf)
-        ./configure --prefix=$INSTALL_PATH/host --with-system-ffi
+        HOST_ARCH="$(uname -m)"
+        CROSSCOMPILE=1
+        if [[ $HOST_ARCH == *"arm"* ]]
+        then
+          echo "Detected arm arch so not cross compiling";
+          CROSSCOMPILE=0
+        else
+          echo "Detected non arm arch so cross compiling";
+        fi
+
+        cd ../$OPENSSL
+        ./Configure $OS-$ARCH -fPIC no-shared --prefix=$INSTALL_PATH/host
+        make -s -j $MAKEJ
+        make install_sw
+        make distclean
+        if [ $CROSSCOMPILE -eq 1 ]
+        then
+          ./Configure linux-generic32 -march=armv6 -mfpu=vfp -mfloat-abi=hard -fPIC no-shared --prefix=$INSTALL_PATH --cross-compile-prefix=arm-linux-gnueabihf-
+        else
+          ./Configure linux-generic32 -fPIC no-shared --prefix=$INSTALL_PATH
+        fi
+        make -s -j $MAKEJ
+        make install_sw
+        make distclean
+        cd ../Python-$CPYTHON_VERSION
+        ./configure --prefix=$INSTALL_PATH/host --with-system-ffi --with-openssl=$INSTALL_PATH/host
         make -j $MAKEJ
         make install
         make distclean
         export PATH=$INSTALL_PATH/host/bin/:$PATH
-        CC="arm-linux-gnueabihf-gcc -std=c99 -march=armv6 -mfpu=vfp -mfloat-abi=hard" ./configure --prefix=$INSTALL_PATH --host=arm-linux-gnueabihf --build=$(uname -m)-pc-linux-gnu --enable-shared --with-system-ffi LDFLAGS='-s -Wl,-rpath,\$$ORIGIN/,-rpath,\$$ORIGIN/../,-rpath,\$$ORIGIN/../lib/' ac_cv_file__dev_ptmx=no ac_cv_file__dev_ptc=no --disable-ipv6
+        CC="arm-linux-gnueabihf-gcc -std=c99 -march=armv6 -mfpu=vfp -mfloat-abi=hard" ./configure --prefix=$INSTALL_PATH --host=arm-linux-gnueabihf --build=$(uname -m)-pc-linux-gnu --enable-shared --with-system-ffi --with-openssl=$INSTALL_PATH LDFLAGS='-s -Wl,-rpath,\$$ORIGIN/,-rpath,\$$ORIGIN/../,-rpath,\$$ORIGIN/../lib/' ac_cv_file__dev_ptmx=no ac_cv_file__dev_ptc=no --disable-ipv6
         make -j $MAKEJ
         make install
         ;;
     linux-arm64)
-        ./configure --prefix=$INSTALL_PATH/host --with-system-ffi
+        CFLAGS="-march=armv8-a+crypto -mcpu=cortex-a57+crypto"
+        cd ../$OPENSSL
+        ./Configure $OS-$ARCH -fPIC no-shared --prefix=$INSTALL_PATH/host
+        make -s -j $MAKEJ
+        make install_sw
+        make distclean
+        ./Configure linux-aarch64 -fPIC --prefix=$INSTALL_PATH --cross-compile-prefix=aarch64-linux-gnu- "$CFLAGS" no-shared
+        make -s -j $MAKEJ
+        make install_sw
+        make distclean
+        cd ../Python-$CPYTHON_VERSION
+        ./configure --prefix=$INSTALL_PATH/host --with-system-ffi --with-openssl=$INSTALL_PATH/host
         make -j $MAKEJ
         make install
         make distclean
         export PATH=$INSTALL_PATH/host/bin/:$PATH
-        CC="aarch64-linux-gnu-gcc -mabi=lp64" ./configure --prefix=$INSTALL_PATH --host=aarch64-linux-gnu --build=$(uname -m)-pc-linux-gnu --enable-shared --with-system-ffi LDFLAGS='-s -Wl,-rpath,\$$ORIGIN/,-rpath,\$$ORIGIN/../,-rpath,\$$ORIGIN/../lib/' ac_cv_file__dev_ptmx=no ac_cv_file__dev_ptc=no --disable-ipv6
+        CC="aarch64-linux-gnu-gcc -mabi=lp64 $CFLAGS" ./configure --prefix=$INSTALL_PATH --host=aarch64-linux-gnu --build=$(uname -m)-pc-linux-gnu --enable-shared --with-system-ffi --with-openssl=$INSTALL_PATH LDFLAGS='-s -Wl,-rpath,\$$ORIGIN/,-rpath,\$$ORIGIN/../,-rpath,\$$ORIGIN/../lib/' ac_cv_file__dev_ptmx=no ac_cv_file__dev_ptc=no --disable-ipv6
         make -j $MAKEJ
         make install
         ;;
     linux-ppc64le)
-        ./configure --prefix=$INSTALL_PATH/host --with-system-ffi
+        MACHINE_TYPE=$( uname -m )
+        cd ../$OPENSSL
+        ./Configure $OS-$ARCH -fPIC no-shared --prefix=$INSTALL_PATH/host
+        make -s -j $MAKEJ
+        make install_sw
+        make distclean
+        if [[ "$MACHINE_TYPE" =~ ppc64 ]]; then
+          ./Configure linux-ppc64le -fPIC no-shared --prefix=$INSTALL_PATH
+        else
+          ./Configure linux-ppc64le -fPIC no-shared --cross-compile-prefix=powerpc64le-linux-gnu- --prefix=$INSTALL_PATH
+        fi
+        make -s -j $MAKEJ
+        make install_sw
+        make distclean
+        cd ../Python-$CPYTHON_VERSION
+        ./configure --prefix=$INSTALL_PATH/host --with-system-ffi --with-openssl=$INSTALL_PATH/host
         make -j $MAKEJ
         make install
         make distclean
         export PATH=$INSTALL_PATH/host/bin/:$PATH
-        CC="powerpc64le-linux-gnu-gcc -m64" ./configure --prefix=$INSTALL_PATH --host=powerpc64le-linux-gnu --build=$(uname -m)-pc-linux-gnu --enable-shared --with-system-ffi LDFLAGS='-s -Wl,-rpath,\$$ORIGIN/,-rpath,\$$ORIGIN/../,-rpath,\$$ORIGIN/../lib/' ac_cv_file__dev_ptmx=no ac_cv_file__dev_ptc=no --disable-ipv6
+        CC="powerpc64le-linux-gnu-gcc -m64" ./configure --prefix=$INSTALL_PATH --host=powerpc64le-linux-gnu --build=$(uname -m)-pc-linux-gnu --enable-shared --with-system-ffi --with-openssl=$INSTALL_PATH LDFLAGS='-s -Wl,-rpath,\$$ORIGIN/,-rpath,\$$ORIGIN/../,-rpath,\$$ORIGIN/../lib/' ac_cv_file__dev_ptmx=no ac_cv_file__dev_ptc=no --disable-ipv6
         make -j $MAKEJ
         make install
         ;;
     linux-x86)
-        CC="gcc -m32" ./configure --prefix=$INSTALL_PATH --enable-shared --with-system-ffi LDFLAGS='-s -Wl,-rpath,\$$ORIGIN/,-rpath,\$$ORIGIN/../,-rpath,\$$ORIGIN/../lib/'
+        cd ../$OPENSSL
+        ./Configure linux-elf -m32 -fPIC no-shared --prefix=$INSTALL_PATH
+        make -s -j $MAKEJ
+        make install_sw
+        cd ../Python-$CPYTHON_VERSION
+        CC="gcc -m32" ./configure --prefix=$INSTALL_PATH --enable-shared --with-system-ffi --with-openssl=$INSTALL_PATH LDFLAGS='-s -Wl,-rpath,\$$ORIGIN/,-rpath,\$$ORIGIN/../,-rpath,\$$ORIGIN/../lib/'
         make -j $MAKEJ
         make install
         ;;
     linux-x86_64)
-        CC="gcc -m64" ./configure --prefix=$INSTALL_PATH --enable-shared --with-system-ffi LDFLAGS='-s -Wl,-rpath,\$$ORIGIN/,-rpath,\$$ORIGIN/../,-rpath,\$$ORIGIN/../lib/'
+        cd ../$OPENSSL
+        ./Configure linux-x86_64 -fPIC no-shared --prefix=$INSTALL_PATH
+        make -s -j $MAKEJ
+        make install_sw
+        cd ../Python-$CPYTHON_VERSION
+        CC="gcc -m64" ./configure --prefix=$INSTALL_PATH --enable-shared --with-system-ffi --with-openssl=$INSTALL_PATH LDFLAGS='-s -Wl,-rpath,\$$ORIGIN/,-rpath,\$$ORIGIN/../,-rpath,\$$ORIGIN/../lib/'
         make -j $MAKEJ
         make install
         ;;
     macosx-*)
-        ./configure --prefix=$INSTALL_PATH --enable-shared --with-openssl="$(brew --prefix openssl@1.1)" LDFLAGS='-s -Wl,-rpath,@loader_path/,-rpath,@loader_path/../,-rpath,@loader_path/../lib/'
+        cd ../$OPENSSL
+        ./Configure darwin64-x86_64-cc -fPIC no-shared --prefix=$INSTALL_PATH
+        make -s -j $MAKEJ
+        make install_sw
+        cd ../Python-$CPYTHON_VERSION
+        ./configure --prefix=$INSTALL_PATH --enable-shared --with-openssl=$INSTALL_PATH LDFLAGS='-s -Wl,-rpath,@loader_path/,-rpath,@loader_path/../,-rpath,@loader_path/../lib/'
         sedinplace 's:-install_name,$(prefix)/lib/:-install_name,@rpath/:g' Makefile
         make -j $MAKEJ
         make install
