dd0a8ddd5c179de907bdc53302e099563a144e37
Syed Aqeel Haider <haideraqeel86@hotmail.com>
2020-08-18 19:40:23 +0500

    * Fix JPMS modules for OpenCV, CPython, LLVM, Tesseract, Qt (pull #928)

--- qt/src/main/java9/module-info.java
+++ qt/src/main/java9/module-info.java
@@ -1,6 +1,7 @@
 module org.bytedeco.qt {
   requires transitive org.bytedeco.javacpp;
   exports org.bytedeco.qt.global;
+  exports org.bytedeco.qt.helper;
   exports org.bytedeco.qt.presets;
   exports org.bytedeco.qt.Qt5Core;
   exports org.bytedeco.qt.Qt5Gui;
--- tesseract/src/main/java9/module-info.java
+++ tesseract/src/main/java9/module-info.java
@@ -3,5 +3,6 @@ module org.bytedeco.tesseract {
   requires transitive org.bytedeco.leptonica;
   exports org.bytedeco.tesseract.global;
   exports org.bytedeco.tesseract.presets;
+  exports org.bytedeco.tesseract.program;
   exports org.bytedeco.tesseract;
 }
