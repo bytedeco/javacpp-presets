d760b8fb8bb0936fe9ef53cffb18aa20e9997ba3
Syed Aqeel Haider <haideraqeel86@hotmail.com>
2020-08-18 19:40:23 +0500

    * Fix JPMS modules for OpenCV, CPython, LLVM, Tesseract, Qt (pull #928)

--- cpython/src/main/java9/module-info.java
+++ cpython/src/main/java9/module-info.java
@@ -1,6 +1,7 @@
 module org.bytedeco.cpython {
   requires transitive org.bytedeco.javacpp;
   exports org.bytedeco.cpython.global;
+  exports org.bytedeco.cpython.helper;
   exports org.bytedeco.cpython.presets;
   exports org.bytedeco.cpython;
 }
--- llvm/src/main/java9/module-info.java
+++ llvm/src/main/java9/module-info.java
@@ -2,6 +2,7 @@ module org.bytedeco.llvm {
   requires transitive org.bytedeco.javacpp;
   exports org.bytedeco.llvm.global;
   exports org.bytedeco.llvm.presets;
+  exports org.bytedeco.llvm.program;
   exports org.bytedeco.llvm.clang;
   exports org.bytedeco.llvm.LLVM;
 }
