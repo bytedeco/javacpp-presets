b561049835b06dcf5326534c4a3ef6f2c444b7b7
Samuel Audet <samuel.audet@gmail.com>
2020-09-12 02:38:29 +0900

    Update download URL for MXNet

--- mxnet/cppbuild.sh
+++ mxnet/cppbuild.sh
@@ -29,8 +29,8 @@ if [[ "$EXTENSION" == *gpu ]]; then
     export ACTUAL_GCC_HOST_COMPILER_PATH=$(which -a gcc | grep -v /ccache/ | head -1) # skip ccache
 fi
 
-MXNET_VERSION=1.7.0.rc1
-download https://github.com/apache/incubator-mxnet/releases/download/$MXNET_VERSION/apache-mxnet-src-$MXNET_VERSION-incubating.tar.gz apache-mxnet-src-$MXNET_VERSION-incubating.tar.gz
+MXNET_VERSION=1.7.0
+download https://downloads.apache.org/incubator/mxnet/$MXNET_VERSION/apache-mxnet-src-$MXNET_VERSION-incubating.tar.gz apache-mxnet-src-$MXNET_VERSION-incubating.tar.gz
 
 mkdir -p "$PLATFORM$EXTENSION"
 cd "$PLATFORM$EXTENSION"
