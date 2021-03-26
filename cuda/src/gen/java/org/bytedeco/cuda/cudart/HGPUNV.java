9220d410247cec3c27dab67b171bef1f698c1187
Park JeongHwan <dev.parkjeonghwan@gmail.com>
2021-03-26 20:29:23 +0900

    * Include `cudaGL.h` and `cuda_gl_interop.h` header files in presets for CUDA (pull #1027)

--- cuda/src/gen/java/org/bytedeco/cuda/cudart/HGPUNV.java
+++ cuda/src/gen/java/org/bytedeco/cuda/cudart/HGPUNV.java
@@ -10,11 +10,13 @@ import static org.bytedeco.javacpp.presets.javacpp.*;
 
 import static org.bytedeco.cuda.global.cudart.*;
 
-/** CUDA memory pool */
-@Opaque @Properties(inherit = org.bytedeco.cuda.presets.cudart.class)
-public class CUmemPoolHandle_st extends Pointer {
+
+// #ifdef _WIN32
+// #ifndef WGL_NV_gpu_affinity
+@Namespace @Name("void") @Opaque @Properties(inherit = org.bytedeco.cuda.presets.cudart.class)
+public class HGPUNV extends Pointer {
     /** Empty constructor. Calls {@code super((Pointer)null)}. */
-    public CUmemPoolHandle_st() { super((Pointer)null); }
+    public HGPUNV() { super((Pointer)null); }
     /** Pointer cast constructor. Invokes {@link Pointer#Pointer(Pointer)}. */
-    public CUmemPoolHandle_st(Pointer p) { super(p); }
+    public HGPUNV(Pointer p) { super(p); }
 }
