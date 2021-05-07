70e5e96040b12eefdca8e74c123ed67311879675
Samuel Audet <samuel.audet@gmail.com>
2021-05-07 22:14:15 +0900

    * Upgrade presets for Arrow 4.0.0

--- arrow/src/main/java/org/bytedeco/arrow/presets/arrow_dataset.java
+++ arrow/src/main/java/org/bytedeco/arrow/presets/arrow_dataset.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2020 Samuel Audet
+ * Copyright (C) 2020-2021 Samuel Audet
  *
  * Licensed either under the Apache License, Version 2.0, or (at your option)
  * under the terms of the GNU General Public License as published by
@@ -44,6 +44,7 @@ import org.bytedeco.javacpp.tools.InfoMapper;
         @Platform(
             include = {
                 "arrow/util/iterator.h",
+                "arrow/util/async_generator.h",
                 "arrow/result.h",
                 "arrow/dataset/api.h",
                 "arrow/dataset/expression.h",
@@ -61,7 +62,7 @@ import org.bytedeco.javacpp.tools.InfoMapper;
                 "arrow/dataset/file_parquet.h",
 //                "arrow/dataset/filter.h",
             },
-            link = "arrow_dataset@.300"
+            link = "arrow_dataset@.400"
         ),
     },
     target = "org.bytedeco.arrow_dataset",
@@ -73,11 +74,13 @@ public class arrow_dataset implements InfoMapper {
     public void map(InfoMap infoMap) {
         infoMap.put(new Info("ARROW_DS_EXPORT").cppTypes().annotations())
                .put(new Info("std::enable_shared_from_this<arrow::dataset::Dataset>",
+                             "std::enable_shared_from_this<arrow::dataset::Fragment>",
                              "std::enable_shared_from_this<arrow::dataset::FileFormat>",
                              "arrow::dataset::FileSource::CustomOpenWithCompression",
-                             "arrow::util::optional<arrow::dataset::KeyValuePartitioning::Key>", "arrow::util::optional<Key>",
                              "arrow::util::EqualityComparable<arrow::dataset::RowGroupInfo>",
                              "arrow::util::EqualityComparable<arrow::Iterator<std::shared_ptr<arrow::dataset::Fragment> > >",
+                             "arrow::util::EqualityComparable<arrow::Iterator<arrow::dataset::TaggedRecordBatch> >",
+                             "arrow::util::EqualityComparable<arrow::Iterator<arrow::dataset::EnumeratedRecordBatch> >",
                              "arrow::util::EqualityComparable<arrow::Iterator<std::unique_ptr<arrow::dataset::ScanTask> > >",
                              "arrow::util::EqualityComparable<arrow::Result<std::shared_ptr<arrow::dataset::Dataset> > >",
                              "arrow::util::EqualityComparable<arrow::Result<std::shared_ptr<arrow::dataset::DatasetFactory> > >",
@@ -99,9 +102,24 @@ public class arrow_dataset implements InfoMapper {
                              "arrow::util::EqualityComparable<arrow::Result<std::vector<std::shared_ptr<arrow::dataset::Fragment> > > >",
                              "arrow::util::EqualityComparable<arrow::Result<std::vector<std::unique_ptr<arrow::dataset::ScanTask> > > >",
                              "arrow::util::EqualityComparable<arrow::Result<arrow::dataset::Expression> >",
+                             "arrow::util::EqualityComparable<arrow::Result<arrow::dataset::RecordBatchGenerator> >",
+                             "arrow::util::EqualityComparable<arrow::Result<arrow::dataset::TaggedRecordBatchGenerator> >",
+                             "arrow::util::EqualityComparable<arrow::Result<arrow::dataset::EnumeratedRecordBatchGenerator> >",
+                             "arrow::util::EqualityComparable<arrow::Result<arrow::dataset::TaggedRecordBatch> >",
+                             "arrow::util::EqualityComparable<arrow::Result<arrow::dataset::EnumeratedRecordBatch> >",
+                             "arrow::util::EqualityComparable<arrow::Result<std::vector<arrow::dataset::TaggedRecordBatch> > >",
+                             "arrow::util::EqualityComparable<arrow::Result<std::vector<arrow::dataset::EnumeratedRecordBatch> > >",
+                             "arrow::util::EqualityComparable<arrow::Result<arrow::dataset::TaggedRecordBatchIterator> >",
+                             "arrow::util::EqualityComparable<arrow::Result<arrow::dataset::EnumeratedRecordBatchIterator> >",
                              "arrow::util::EqualityComparable<arrow::Result<arrow::dataset::Partitioning::PartitionedBatches> >",
                              "arrow::util::EqualityComparable<arrow::Result<arrow::Iterator<std::shared_ptr<arrow::dataset::Fragment> > > >",
                              "arrow::util::EqualityComparable<arrow::Result<arrow::dataset::ScanTaskIterator> >").cast().pointerTypes("Pointer"))
+
+               .put(new Info("arrow::util::optional<std::string>").pointerTypes("StringOptional").define())
+               .put(new Info("arrow::util::optional<arrow::Compression::type>").pointerTypes("CompressionTypeOptional").define())
+               .put(new Info("arrow::dataset::KeyValuePartitioning::Key").pointerTypes("KeyValuePartitioning.Key"))
+               .put(new Info("arrow::util::optional<arrow::dataset::KeyValuePartitioning::Key>", "arrow::util::optional<Key>").pointerTypes("KeyOptional").define())
+
                .put(new Info("std::unordered_set<std::string>").pointerTypes("StringUnorderedSet").define())
                .put(new Info("std::shared_ptr<arrow::dataset::Dataset>").annotations("@SharedPtr").pointerTypes("Dataset"))
                .put(new Info("std::shared_ptr<arrow::dataset::DatasetFactory>").annotations("@SharedPtr").pointerTypes("DatasetFactory"))
@@ -124,6 +142,8 @@ public class arrow_dataset implements InfoMapper {
                .put(new Info("std::vector<std::shared_ptr<arrow::dataset::FileFragment> >").pointerTypes("FileFragmentVector").define())
                .put(new Info("std::vector<std::unique_ptr<arrow::dataset::ScanTask> >").pointerTypes("ScanTaskVector").define())
 //               .put(new Info("std::vector<std::shared_ptr<arrow::dataset::SourceFactory> >").pointerTypes("SourceFactoryVector").define())
+
+               .put(new Info("arrow::Enumerated<std::shared_ptr<arrow::dataset::Fragment> >").pointerTypes("FragmentEnumerated").define())
                .put(new Info("arrow::Result<std::shared_ptr<arrow::dataset::Dataset> >").pointerTypes("DatasetResult").define())
                .put(new Info("arrow::Result<std::shared_ptr<arrow::dataset::DatasetFactory> >").pointerTypes("DatasetFactoryResult").define())
 //               .put(new Info("arrow::Result<std::shared_ptr<arrow::dataset::Expression> >").pointerTypes("ExpressionResult").define())
@@ -144,6 +164,19 @@ public class arrow_dataset implements InfoMapper {
 
                .put(new Info("arrow::compute::Function").pointerTypes("org.bytedeco.arrow.Function"))
                .put(new Info("arrow::Result<arrow::dataset::Expression>").pointerTypes("ExpressionResult").define())
+               .put(new Info("arrow::Result<arrow::dataset::RecordBatchGenerator>").pointerTypes("RecordBatchGeneratorResult").define())
+               .put(new Info("arrow::Result<arrow::dataset::TaggedRecordBatchGenerator>").pointerTypes("TaggedRecordBatchGeneratorResult").define())
+               .put(new Info("arrow::Result<arrow::dataset::EnumeratedRecordBatchGenerator>").pointerTypes("EnumeratedRecordBatchGeneratorResult").define())
+               .put(new Info("arrow::Result<arrow::dataset::TaggedRecordBatch>").pointerTypes("TaggedRecordBatchResult").define())
+               .put(new Info("arrow::Result<std::vector<arrow::dataset::TaggedRecordBatch> >").pointerTypes("TaggedRecordBatchVectorResult").define())
+               .put(new Info("arrow::Result<arrow::dataset::EnumeratedRecordBatch>").pointerTypes("TaggedRecordBatchResult").define())
+               .put(new Info("arrow::Result<std::vector<arrow::dataset::EnumeratedRecordBatch> >").pointerTypes("EnumeratedRecordBatchResult").define())
+               .put(new Info("arrow::Result<arrow::dataset::TaggedRecordBatchIterator>").pointerTypes("TaggedRecordBatchIteratorResult").define())
+               .put(new Info("arrow::Iterator<arrow::dataset::TaggedRecordBatch>").pointerTypes("TaggedRecordBatchIterator").define())
+               .put(new Info("arrow::Iterator<arrow::dataset::TaggedRecordBatch>::RangeIterator").pointerTypes("TaggedRecordBatchIterator.RangeIterator").define())
+               .put(new Info("arrow::Result<arrow::dataset::EnumeratedRecordBatchIterator>").pointerTypes("EnumeratedRecordBatchIteratorResult").define())
+               .put(new Info("arrow::Iterator<arrow::dataset::EnumeratedRecordBatch>").pointerTypes("EnumeratedRecordBatchIterator").define())
+               .put(new Info("arrow::Iterator<arrow::dataset::EnumeratedRecordBatch>::RangeIterator").pointerTypes("EnumeratedRecordBatchIterator.RangeIterator").define())
                .put(new Info("arrow::dataset::Partitioning::PartitionedBatches", "PartitionedBatches>").pointerTypes("Partitioning.PartitionedBatches"))
                .put(new Info("arrow::Result<arrow::dataset::Partitioning::PartitionedBatches>", "arrow::Result<PartitionedBatches>").pointerTypes("PartitionedBatchesResult").define())
 //               .put(new Info("arrow::Result<std::shared_ptr<arrow::dataset::Source> >").pointerTypes("SourceResult").define())
@@ -172,10 +205,19 @@ public class arrow_dataset implements InfoMapper {
                              "arrow::dataset::ExpressionImpl<arrow::dataset::BinaryExpression,ComparisonExpression,arrow::dataset::ExpressionType::COMPARISON>").pointerTypes("ComparisonExpressionImpl").define())
                .put(new Info("arrow::dataset::DiscoverSource", "arrow::dataset::RowGroupStatisticsAsExpression",
                              "arrow::dataset::WritePlan::fragment_or_partition_expressions", "arrow::dataset::string_literals::operator \"\"_")/*.javaNames("quote")*/.skip())
+
                .put(new Info("std::function<arrow::Result<std::shared_ptr<arrow::io::RandomAccessFile> >(arrow::Compression::type)>",
                              "std::function<arrow::Result<std::shared_ptr<io::RandomAccessFile> >(Compression::type)>").pointerTypes("CustomOpenWithCompression").define())
                .put(new Info("std::function<arrow::Status(const arrow::dataset::Expression&)>",
                              "std::function<arrow::Status(const Expression&)>").pointerTypes("ExpressionVisitor").define())
+               .put(new Info("std::function<arrow::Status(arrow::dataset::TaggedRecordBatch)>",
+                             "std::function<arrow::Status(TaggedRecordBatch)>").pointerTypes("TaggedRecordBatchVisitor").define())
+               .put(new Info("std::function<arrow::Future<std::shared_ptr<RecordBatch> >()>",
+                             "std::function<arrow::Future<std::shared_ptr<arrow::RecordBatch> >()>").pointerTypes("RecordBatchGenerator").define())
+               .put(new Info("std::function<arrow::Future<TaggedRecordBatchGenerator>()>",
+                             "std::function<arrow::Future<arrow::dataset::TaggedRecordBatchGenerator>()>").pointerTypes("TaggedRecordBatchGenerator").define())
+               .put(new Info("std::function<arrow::Future<EnumeratedRecordBatch>()>",
+                             "std::function<arrow::Future<arrow::dataset::EnumeratedRecordBatch>()>").pointerTypes("EnumeratedRecordBatchGenerator").define())
         ;
     }
 
@@ -195,5 +237,41 @@ public class arrow_dataset implements InfoMapper {
         protected ExpressionVisitor() { allocate(); }
         private native void allocate();
         public native @ByVal @Cast("arrow::Status*") Pointer call(@ByRef @Cast("const arrow::dataset::Expression*") Pointer node);
+    }
+
+    public static class TaggedRecordBatchVisitor extends FunctionPointer {
+        static { Loader.load(); }
+        /** Pointer cast constructor. Invokes {@link Pointer#Pointer(Pointer)}. */
+        public    TaggedRecordBatchVisitor(Pointer p) { super(p); }
+        protected TaggedRecordBatchVisitor() { allocate(); }
+        private native void allocate();
+        public native @ByVal @Cast("arrow::Status*") Pointer call(@ByVal @Cast("const arrow::dataset::TaggedRecordBatch*") Pointer node);
+    }
+
+    public static class RecordBatchGenerator extends FunctionPointer {
+        static { Loader.load(); }
+        /** Pointer cast constructor. Invokes {@link Pointer#Pointer(Pointer)}. */
+        public    RecordBatchGenerator(Pointer p) { super(p); }
+        protected RecordBatchGenerator() { allocate(); }
+        private native void allocate();
+        public native @ByVal @Cast("arrow::Future<std::shared_ptr<arrow::RecordBatch> >*") Pointer call();
+    }
+
+    public static class TaggedRecordBatchGenerator extends FunctionPointer {
+        static { Loader.load(); }
+        /** Pointer cast constructor. Invokes {@link Pointer#Pointer(Pointer)}. */
+        public    TaggedRecordBatchGenerator(Pointer p) { super(p); }
+        protected TaggedRecordBatchGenerator() { allocate(); }
+        private native void allocate();
+        public native @ByVal @Cast("arrow::Future<arrow::dataset::TaggedRecordBatch>*") Pointer call();
+    }
+
+    public static class EnumeratedRecordBatchGenerator extends FunctionPointer {
+        static { Loader.load(); }
+        /** Pointer cast constructor. Invokes {@link Pointer#Pointer(Pointer)}. */
+        public    EnumeratedRecordBatchGenerator(Pointer p) { super(p); }
+        protected EnumeratedRecordBatchGenerator() { allocate(); }
+        private native void allocate();
+        public native @ByVal @Cast("arrow::Future<arrow::dataset::EnumeratedRecordBatch>*") Pointer call();
     }
 }
