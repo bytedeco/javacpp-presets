714044b513dc72b5e79777778fbf4ba38c7181b0
Jarek Sacha <jpsacha@gmail.com>
2021-05-06 21:42:24 -0400

    Remove Spinnaker Linux debug statements

--- spinnaker/cppbuild.sh
+++ spinnaker/cppbuild.sh
@@ -25,10 +25,6 @@ case $PLATFORM in
     linux-x86*)
         if [[ ! -d "/usr/include/spinnaker/" ]] && [[ ! -d "/opt/spinnaker/include/" ]]; then
             echo "Please install Spinnaker under the default installation directory: /usr/include/spinnaker or /opt/spinnaker/include"
-            echo "ls /usr/include"
-            ls /usr/include
-            echo "ls /opt/spinnaker/include"
-            ls /opt/spinnaker/include
             exit 1
         fi
         ;;
