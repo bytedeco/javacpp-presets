09da5f6e1dd14376ef0c35fa41b91800de0d4665
Samuel Audet <samuel.audet@gmail.com>
2021-05-31 13:36:55 +0900

    * Add presets for TensorFlow Lite 2.5.0

---
+++ tensorflow-lite/src/main/java9/module-info.java
@@ -0,0 +1,6 @@
+module org.bytedeco.tensorflowlite {
+  requires transitive org.bytedeco.javacpp;
+  exports org.bytedeco.tensorflowlite.global;
+  exports org.bytedeco.tensorflowlite.presets;
+  exports org.bytedeco.tensorflowlite;
+}
