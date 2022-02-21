cc3eaa13bb07acd49cdfe2a593ee4ddfc85e60d5
Andrey Krainyak <mode.andrew@gmail.com>
2022-02-21 23:06:24 +0800

    Build bullet preset on android platforms

--- bullet/cppbuild.sh
+++ bullet/cppbuild.sh
@@ -18,11 +18,108 @@ INSTALL_PATH=`pwd`
 echo "Decompressing archives..."
 unzip -qo ../bullet-$BULLET_VERSION.zip
 
+cd bullet3-$BULLET_VERSION
+[ -d .build ] || mkdir .build
+cd .build
+
 case $PLATFORM in
+    android-arm)
+        cmake \
+            -DANDROID_ABI=armeabi-v7a \
+            -DANDROID_NATIVE_API_LEVEL=24 \
+            -DBUILD_BULLET2_DEMOS=OFF \
+            -DBUILD_CLSOCKET=OFF \
+            -DBUILD_CPU_DEMOS=OFF \
+            -DBUILD_EGL=OFF \
+            -DBUILD_ENET=OFF \
+            -DBUILD_EXTRAS=OFF \
+            -DBUILD_OPENGL3_DEMOS=OFF \
+            -DBUILD_SHARED_LIBS=ON \
+            -DBUILD_UNIT_TESTS=OFF \
+            -DCMAKE_BUILD_TYPE=Release \
+            -DCMAKE_INSTALL_PREFIX=$INSTALL_PATH \
+            -DCMAKE_TOOLCHAIN_FILE=${PLATFORM_ROOT}/build/cmake/android.toolchain.cmake \
+            -DENABLE_VHACD=OFF \
+            -DUSE_DOUBLE_PRECISION=OFF \
+            -DUSE_GLUT=OFF \
+            -DUSE_GRAPHICAL_BENCHMARK=OFF \
+            ..
+        make -j $MAKEJ
+        make install/strip
+        ;;
+    android-arm64)
+        cmake \
+            -DANDROID_ABI=arm64-v8a \
+            -DANDROID_NATIVE_API_LEVEL=24 \
+            -DBUILD_BULLET2_DEMOS=OFF \
+            -DBUILD_CLSOCKET=OFF \
+            -DBUILD_CPU_DEMOS=OFF \
+            -DBUILD_EGL=OFF \
+            -DBUILD_ENET=OFF \
+            -DBUILD_EXTRAS=OFF \
+            -DBUILD_OPENGL3_DEMOS=OFF \
+            -DBUILD_SHARED_LIBS=ON \
+            -DBUILD_UNIT_TESTS=OFF \
+            -DCMAKE_BUILD_TYPE=Release \
+            -DCMAKE_INSTALL_PREFIX=$INSTALL_PATH \
+            -DCMAKE_TOOLCHAIN_FILE=${PLATFORM_ROOT}/build/cmake/android.toolchain.cmake \
+            -DENABLE_VHACD=OFF \
+            -DUSE_DOUBLE_PRECISION=OFF \
+            -DUSE_GLUT=OFF \
+            -DUSE_GRAPHICAL_BENCHMARK=OFF \
+            ..
+        make -j $MAKEJ
+        make install/strip
+        ;;
+    android-x86)
+        cmake \
+            -DANDROID_ABI=x86 \
+            -DANDROID_NATIVE_API_LEVEL=24 \
+            -DBUILD_BULLET2_DEMOS=OFF \
+            -DBUILD_CLSOCKET=OFF \
+            -DBUILD_CPU_DEMOS=OFF \
+            -DBUILD_EGL=OFF \
+            -DBUILD_ENET=OFF \
+            -DBUILD_EXTRAS=OFF \
+            -DBUILD_OPENGL3_DEMOS=OFF \
+            -DBUILD_SHARED_LIBS=ON \
+            -DBUILD_UNIT_TESTS=OFF \
+            -DCMAKE_BUILD_TYPE=Release \
+            -DCMAKE_INSTALL_PREFIX=$INSTALL_PATH \
+            -DCMAKE_TOOLCHAIN_FILE=${PLATFORM_ROOT}/build/cmake/android.toolchain.cmake \
+            -DENABLE_VHACD=OFF \
+            -DUSE_DOUBLE_PRECISION=OFF \
+            -DUSE_GLUT=OFF \
+            -DUSE_GRAPHICAL_BENCHMARK=OFF \
+            ..
+        make -j $MAKEJ
+        make install/strip
+        ;;
+    android-x86_64)
+        cmake \
+            -DANDROID_ABI=x86_64 \
+            -DANDROID_NATIVE_API_LEVEL=24 \
+            -DBUILD_BULLET2_DEMOS=OFF \
+            -DBUILD_CLSOCKET=OFF \
+            -DBUILD_CPU_DEMOS=OFF \
+            -DBUILD_EGL=OFF \
+            -DBUILD_ENET=OFF \
+            -DBUILD_EXTRAS=OFF \
+            -DBUILD_OPENGL3_DEMOS=OFF \
+            -DBUILD_SHARED_LIBS=ON \
+            -DBUILD_UNIT_TESTS=OFF \
+            -DCMAKE_BUILD_TYPE=Release \
+            -DCMAKE_INSTALL_PREFIX=$INSTALL_PATH \
+            -DCMAKE_TOOLCHAIN_FILE=${PLATFORM_ROOT}/build/cmake/android.toolchain.cmake \
+            -DENABLE_VHACD=OFF \
+            -DUSE_DOUBLE_PRECISION=OFF \
+            -DUSE_GLUT=OFF \
+            -DUSE_GRAPHICAL_BENCHMARK=OFF \
+            ..
+        make -j $MAKEJ
+        make install/strip
+        ;;
     linux-x86_64)
-        cd bullet3-$BULLET_VERSION
-        [ -d .build ] || mkdir .build
-        cd .build
         cmake \
             -DBUILD_BULLET2_DEMOS=OFF \
             -DBUILD_CLSOCKET=OFF \
@@ -42,11 +139,10 @@ case $PLATFORM in
             ..
         make -j $MAKEJ
         make install/strip
-        cd ../..
         ;;
     *)
         echo "Error: Platform \"$PLATFORM\" is not supported"
         ;;
 esac
 
-cd ..
+cd ../../..
