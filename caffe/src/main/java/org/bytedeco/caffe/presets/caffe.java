c6a63ad44c00ca677b974bd1d1211b7def8cd5c0
Samuel Audet <samuel.audet@gmail.com>
2022-03-08 10:36:50 +0900

    Ensure `zlibwapi.dll` gets preloaded in presets that need it for cuDNN (issue bytedeco/javacv#1767)

--- caffe/src/main/java/org/bytedeco/caffe/presets/caffe.java
+++ caffe/src/main/java/org/bytedeco/caffe/presets/caffe.java
@@ -80,6 +80,9 @@ public class caffe implements LoadEnabled, InfoMapper {
             return;
         }
         int i = 0;
+        if (platform.startsWith("windows")) {
+            preloads.add(i++, "zlibwapi");
+        }
         String[] libs = {"cudart", "cublasLt", "cublas", "curand", "cudnn",
                          "cudnn_ops_infer", "cudnn_ops_train", "cudnn_adv_infer",
                          "cudnn_adv_train", "cudnn_cnn_infer", "cudnn_cnn_train"};
