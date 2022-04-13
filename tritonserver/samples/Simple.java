6990a5fef952b859bb6d4fae2fec1001994a51af
jackyh <franc78@126.com>
2022-04-13 14:17:08 +0800

    Remove unneeded string allocations in samples for Triton Inference Server (pull #1169)
    Co-authored-by: jackhe <jackhe@nvidia.com>
    Co-authored-by: Samuel Audet <samuel.audet@gmail.com>

--- tritonserver/samples/Simple.java
+++ tritonserver/samples/Simple.java
@@ -197,7 +197,7 @@ public class Simple {
             // releasing the buffer.
             if (!allocated_ptr.isNull()) {
               buffer.put(0, allocated_ptr);
-              buffer_userp.put(0, new BytePointer(tensor_name));
+              buffer_userp.put(0, Loader.newGlobalRef(tensor_name));
               System.out.println("allocated " + byte_size + " bytes in "
                                + TRITONSERVER_MemoryTypeString(actual_memory_type.get())
                                + " for result tensor " + tensor_name);
@@ -213,16 +213,16 @@ public class Simple {
             TRITONSERVER_ResponseAllocator allocator, Pointer buffer, Pointer buffer_userp,
             long byte_size, int memory_type, long memory_type_id)
         {
-          BytePointer name = null;
+          String name = null;
           if (buffer_userp != null) {
-            name = new BytePointer(buffer_userp);
+            name = (String)Loader.accessGlobalRef(buffer_userp);
           } else {
-            name = new BytePointer("<unknown>");
+            name = "<unknown>";
           }
 
           System.out.println("Releasing buffer " + buffer + " of size " + byte_size
                            + " in " + TRITONSERVER_MemoryTypeString(memory_type)
-                           + " for result '" + name.getString() + "'");
+                           + " for result '" + name + "'");
           switch (memory_type) {
             case TRITONSERVER_MEMORY_CPU:
               Pointer.free(buffer);
@@ -254,7 +254,7 @@ public class Simple {
               break;
           }
 
-          name.deallocate();
+          Loader.deleteGlobalRef(buffer_userp);
 
           return null;  // Success
         }
