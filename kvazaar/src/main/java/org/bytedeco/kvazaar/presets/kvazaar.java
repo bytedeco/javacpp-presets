fb38d1f2552c9dc680619916b1d7ffe57926e2fc
Brad Hards <bradh@frogmouth.net>
2022-08-21 13:22:52 +1000

    kvazaar: try harder to link on windows

--- kvazaar/src/main/java/org/bytedeco/kvazaar/presets/kvazaar.java
+++ kvazaar/src/main/java/org/bytedeco/kvazaar/presets/kvazaar.java
@@ -7,6 +7,7 @@ import org.bytedeco.javacpp.tools.*;
 @Properties(
     value = {
         @Platform(include = "<kvazaar.h>", link = "kvazaar#")
+        @Platform(value = "windows", preload = {"libkvazaar"}
     },
     target = "org.bytedeco.kvazaar",
     global = "org.bytedeco.kvazaar.global.kvazaar"
