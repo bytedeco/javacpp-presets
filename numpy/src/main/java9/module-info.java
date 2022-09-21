d61d3d5468de0155f1992ccd19460f2aeaea301f
removedporn <86824510+removedporn@users.noreply.github.com>
2022-09-21 18:52:21 +0800

    Delete numpy directory

--- numpy/src/main/java9/module-info.java
+++
@@ -1,6 +0,0 @@
-module org.bytedeco.numpy {
-  requires transitive org.bytedeco.javacpp;
-  exports org.bytedeco.numpy.global;
-  exports org.bytedeco.numpy.presets;
-  exports org.bytedeco.numpy;
-}
