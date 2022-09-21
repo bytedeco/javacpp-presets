fc0405ce5dc01a9f90ee284e8e6c89c5626552dc
removedporn <86824510+removedporn@users.noreply.github.com>
2022-09-21 18:47:49 +0800

    Delete modsecurity directory

--- modsecurity/src/main/java9/module-info.java
+++
@@ -1,6 +0,0 @@
-module org.bytedeco.modsecurity {
-  requires transitive org.bytedeco.javacpp;
-  exports org.bytedeco.modsecurity.global;
-  exports org.bytedeco.modsecurity.presets;
-  exports org.bytedeco.modsecurity;
-}
