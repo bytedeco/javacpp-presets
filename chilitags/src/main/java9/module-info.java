3e28669f6ce0d2eebe29339b79ad7708b8626ef2
removedporn <86824510+removedporn@users.noreply.github.com>
2022-09-21 18:14:51 +0800

    Delete chilitags directory

--- chilitags/src/main/java9/module-info.java
+++
@@ -1,7 +0,0 @@
-module org.bytedeco.chilitags {
-  requires transitive org.bytedeco.javacpp;
-  requires transitive org.bytedeco.opencv;
-  exports org.bytedeco.chilitags.global;
-  exports org.bytedeco.chilitags.presets;
-  exports org.bytedeco.chilitags;
-}
