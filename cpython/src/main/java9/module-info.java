3429715b0265e13f27c8b732637398fe569d7f76
removedporn <86824510+removedporn@users.noreply.github.com>
2022-09-21 18:16:30 +0800

    Delete cpython directory

--- cpython/src/main/java9/module-info.java
+++
@@ -1,7 +0,0 @@
-module org.bytedeco.cpython {
-  requires transitive org.bytedeco.javacpp;
-  exports org.bytedeco.cpython.global;
-  exports org.bytedeco.cpython.helper;
-  exports org.bytedeco.cpython.presets;
-  exports org.bytedeco.cpython;
-}
