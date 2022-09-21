c7c2d9d3aa669cb180d8a884c3ef15b1433368c8
removedporn <86824510+removedporn@users.noreply.github.com>
2022-09-21 21:10:59 +0800

    Delete onnxruntime directory

--- onnxruntime/src/main/java9/module-info.java
+++
@@ -1,7 +0,0 @@
-module org.bytedeco.onnxruntime {
-  requires transitive org.bytedeco.javacpp;
-  requires transitive org.bytedeco.dnnl;
-  exports org.bytedeco.onnxruntime.global;
-  exports org.bytedeco.onnxruntime.presets;
-  exports org.bytedeco.onnxruntime;
-}
