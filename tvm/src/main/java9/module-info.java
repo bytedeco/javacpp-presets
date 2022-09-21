02e846bae209cb31032bc952eb091f2aec9d56bb
removedporn <86824510+removedporn@users.noreply.github.com>
2022-09-21 21:35:39 +0800

    Delete tvm directory

--- tvm/src/main/java9/module-info.java
+++
@@ -1,9 +0,0 @@
-module org.bytedeco.tvm {
-  requires transitive org.bytedeco.javacpp;
-  requires transitive org.bytedeco.dnnl;
-  requires transitive org.bytedeco.llvm;
-  requires transitive org.bytedeco.scipy;
-  exports org.bytedeco.tvm.global;
-  exports org.bytedeco.tvm.presets;
-  exports org.bytedeco.tvm;
-}
