cb7287ab887bed813b939f43eb88ec67b523ae26
removedporn <86824510+removedporn@users.noreply.github.com>
2022-09-21 18:27:15 +0800

    Delete libfreenect directory

--- libfreenect/src/main/java9/module-info.java
+++
@@ -1,6 +0,0 @@
-module org.bytedeco.libfreenect {
-  requires transitive org.bytedeco.javacpp;
-  exports org.bytedeco.libfreenect.global;
-  exports org.bytedeco.libfreenect.presets;
-  exports org.bytedeco.libfreenect;
-}
