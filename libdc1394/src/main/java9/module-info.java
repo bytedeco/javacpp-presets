1681eff2113aaf9e41c0a0dae73e9ce9b0d5a4a7
removedporn <86824510+removedporn@users.noreply.github.com>
2022-09-21 18:26:22 +0800

    Delete libdc1394 directory

--- libdc1394/src/main/java9/module-info.java
+++
@@ -1,6 +0,0 @@
-module org.bytedeco.libdc1394 {
-  requires transitive org.bytedeco.javacpp;
-  exports org.bytedeco.libdc1394.global;
-  exports org.bytedeco.libdc1394.presets;
-  exports org.bytedeco.libdc1394;
-}
