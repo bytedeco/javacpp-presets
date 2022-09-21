d520e63ffc0cead1212d4e79aa48ef46770719db
removedporn <86824510+removedporn@users.noreply.github.com>
2022-09-21 18:14:12 +0800

    Delete caffe directory

--- caffe/src/main/java9/module-info.java
+++
@@ -1,9 +0,0 @@
-module org.bytedeco.caffe {
-  requires transitive org.bytedeco.javacpp;
-  requires transitive org.bytedeco.hdf5;
-  requires transitive org.bytedeco.opencv;
-  requires transitive org.bytedeco.openblas;
-  exports org.bytedeco.caffe.global;
-  exports org.bytedeco.caffe.presets;
-  exports org.bytedeco.caffe;
-}
