7fe23fa80057539693eea10bc03f28d75fdf6049
removedporn <86824510+removedporn@users.noreply.github.com>
2022-09-21 18:29:09 +0800

    Delete librealsense2 directory

--- librealsense2/src/main/java9/module-info.java
+++
@@ -1,6 +0,0 @@
-module org.bytedeco.librealsense2 {
-  requires transitive org.bytedeco.javacpp;
-  exports org.bytedeco.librealsense2.global;
-  exports org.bytedeco.librealsense2.presets;
-  exports org.bytedeco.librealsense2;
-}
