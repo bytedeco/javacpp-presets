c2bd0811e7d089c2c0cf7468acb1a469a5feeb4b
removedporn <86824510+removedporn@users.noreply.github.com>
2022-09-21 18:15:56 +0800

    Delete cpu_features directory

--- cpu_features/src/main/java9/module-info.java
+++
@@ -1,6 +0,0 @@
-module org.bytedeco.cpu_features {
-  requires transitive org.bytedeco.javacpp;
-  exports org.bytedeco.cpu_features.global;
-  exports org.bytedeco.cpu_features.presets;
-  exports org.bytedeco.cpu_features;
-}
