5980e1ec02b4e7f7e2f317bc65a1728c1c9d93af
removedporn <86824510+removedporn@users.noreply.github.com>
2022-09-21 18:21:01 +0800

    Delete flandmark directory

--- flandmark/src/main/java9/module-info.java
+++
@@ -1,7 +0,0 @@
-module org.bytedeco.flandmark {
-  requires transitive org.bytedeco.javacpp;
-  requires transitive org.bytedeco.opencv;
-  exports org.bytedeco.flandmark.global;
-  exports org.bytedeco.flandmark.presets;
-  exports org.bytedeco.flandmark;
-}
