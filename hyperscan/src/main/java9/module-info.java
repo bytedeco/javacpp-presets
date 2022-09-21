6ccc53dfd9c5ea366aa36341229ed6f04f898564
removedporn <86824510+removedporn@users.noreply.github.com>
2022-09-21 18:25:04 +0800

    Delete hyperscan directory

--- hyperscan/src/main/java9/module-info.java
+++
@@ -1,6 +0,0 @@
-module org.bytedeco.hyperscan {
-  requires transitive org.bytedeco.javacpp;
-  exports org.bytedeco.hyperscan;
-  exports org.bytedeco.hyperscan.global;
-  exports org.bytedeco.hyperscan.presets;
-}
