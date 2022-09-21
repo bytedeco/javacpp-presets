89edd0792fc8fcba4f388e96a316628fe32cadbf
removedporn <86824510+removedporn@users.noreply.github.com>
2022-09-21 21:29:05 +0800

    Delete pytorch directory

--- pytorch/src/main/java9/module-info.java
+++
@@ -1,7 +0,0 @@
-module org.bytedeco.pytorch {
-  requires transitive org.bytedeco.javacpp;
-  requires transitive org.bytedeco.openblas;
-  exports org.bytedeco.pytorch.global;
-  exports org.bytedeco.pytorch.presets;
-  exports org.bytedeco.pytorch;
-}
