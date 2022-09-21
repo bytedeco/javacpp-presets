c6a3926081a22bda1613f7d5023fa8128b964393
removedporn <86824510+removedporn@users.noreply.github.com>
2022-09-21 21:35:59 +0800

    Delete videoinput directory

--- videoinput/src/main/java9/module-info.java
+++
@@ -1,6 +0,0 @@
-module org.bytedeco.videoinput {
-  requires transitive org.bytedeco.javacpp;
-  exports org.bytedeco.videoinput.global;
-  exports org.bytedeco.videoinput.presets;
-  exports org.bytedeco.videoinput;
-}
