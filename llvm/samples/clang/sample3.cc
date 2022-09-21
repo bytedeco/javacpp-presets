7b8f125ea7809f6e44fe61cbf914a83f8beef64f
removedporn <86824510+removedporn@users.noreply.github.com>
2022-09-21 18:33:58 +0800

    Delete llvm directory

--- llvm/samples/clang/sample3.cc
+++
@@ -1,5 +0,0 @@
-#include <vector>
-int main(void) {
-  std::vector<int> v;
-  v.push
-}
--- llvm/src/main/java9/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.bytedeco.llvm {
-  requires transitive org.bytedeco.javacpp;
-  exports org.bytedeco.llvm.global;
-  exports org.bytedeco.llvm.presets;
-  exports org.bytedeco.llvm.program;
-  exports org.bytedeco.llvm.clang;
-  exports org.bytedeco.llvm.LLVM;
-}
