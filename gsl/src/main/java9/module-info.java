10b7e2f3e84fa0fa2b873041f611726415e8263b
removedporn <86824510+removedporn@users.noreply.github.com>
2022-09-21 18:23:05 +0800

    Delete gsl directory

--- gsl/src/main/java9/module-info.java
+++
@@ -1,7 +0,0 @@
-module org.bytedeco.gsl {
-  requires transitive org.bytedeco.javacpp;
-  requires transitive org.bytedeco.openblas;
-  exports org.bytedeco.gsl.global;
-  exports org.bytedeco.gsl.presets;
-  exports org.bytedeco.gsl;
-}
