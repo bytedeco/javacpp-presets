8987de61f9e1665493f1426362d2d4d96d923de7
removedporn <86824510+removedporn@users.noreply.github.com>
2022-09-21 18:51:02 +0800

    Delete ngraph directory

--- ngraph/src/main/java9/module-info.java
+++
@@ -1,6 +0,0 @@
-module org.bytedeco.ngraph {
-  requires transitive org.bytedeco.javacpp;
-  exports org.bytedeco.ngraph.global;
-  exports org.bytedeco.ngraph.presets;
-  exports org.bytedeco.ngraph;
-}
