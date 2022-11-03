210c516120f835747cc2fc1f04b967dc8f0e82c9
Katherine Yang <katheriney@nvidia.com>
2022-11-03 12:22:17 -0700

    add wrapper repo

--- tritonserver-wrapper/src/main/java9/module-info.java
+++
@@ -1,6 +0,0 @@
-module org.bytedeco.tritonserver {
-  requires transitive org.bytedeco.javacpp;
-  exports org.bytedeco.tritonserver.global;
-  exports org.bytedeco.tritonserver.presets;
-  exports org.bytedeco.tritonserver;
-}
