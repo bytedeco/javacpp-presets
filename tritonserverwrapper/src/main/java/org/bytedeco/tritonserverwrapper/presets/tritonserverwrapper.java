17c233719f4878a19c85595c7864e7ac75120594
Katherine Yang <katheriney@nvidia.com>
2022-11-03 12:22:17 -0700

    add wrapper repo

---
+++ tritonserverwrapper/src/main/java/org/bytedeco/tritonserverwrapper/presets/tritonserverwrapper.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright (C) 2021 Jack He, Samuel Audet
+ *
+ * Licensed either under the Apache License, Version 2.0, or (at your option)
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation (subject to the "Classpath" exception),
+ * either version 2, or any later version (collectively, the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.gnu.org/licenses/
+ *     http://www.gnu.org/software/classpath/license.html
+ *
+ * or as provided in the LICENSE.txt file that accompanied this code.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.bytedeco.tritonserverwrapper.presets;
+
+import java.util.List;
+import org.bytedeco.javacpp.ClassProperties;
+import org.bytedeco.javacpp.LoadEnabled;
+import org.bytedeco.javacpp.Loader;
+import org.bytedeco.javacpp.annotation.Platform;
+import org.bytedeco.javacpp.annotation.Properties;
+import org.bytedeco.javacpp.tools.Info;
+import org.bytedeco.javacpp.tools.InfoMap;
+import org.bytedeco.javacpp.tools.InfoMapper;
+// import org.bytedeco.tritonserver.presets.tritonserver;
+
+/**
+ *
+ * @author Katherine Yang
+ */
+@Properties(
+    value = {
+        @Platform(
+            value = {"linux-arm64", "linux-ppc64le", "linux-x86_64", "windows-x86_64"},
+            include = {"tritonserver.h", "tritonbackend.h", "tritonrepoagent.h","server_wrapper.h", "infer_requested_output.h", "common.h"},
+            link = "tritonserverwrapper",
+            includepath = {"/opt/tritonserver/include/triton/core/", "/opt/tritonserver/include/", "/usr/include", "/opt/tritonserver/include/triton/developer_tools"},
+            linkpath = {"/opt/tritonserver/lib/"}
+        ),
+        // @Platform(
+        //     value = "windows-x86_64",
+        //     includepath = "C:/Program Files/NVIDIA GPU Computing Toolkit/TritonServer/include/triton/core/",
+        //     linkpath = "C:/Program Files/NVIDIA GPU Computing Toolkit/TritonServer/lib/",
+        //     preloadpath = "C:/Program Files/NVIDIA GPU Computing Toolkit/TritonServer/bin/"
+        // )
+    },
+    target = "org.bytedeco.tritonserverwrapper.tritonserverwrapper",
+    global = "org.bytedeco.tritonserverwrapper.global.tritonserverwrapper"
+)
+public class tritonserverwrapper implements InfoMapper {
+    static { Loader.checkVersion("org.bytedeco", "tritonserverwrapper"); }
+    public void map(InfoMap infoMap) {
+        infoMap.putFirst(new Info().enumerate(false))
+               .put(new Info("bool").cast().valueTypes("boolean").pointerTypes("boolean[]", "BoolPointer"))
+               .put(new Info("const char").pointerTypes("String", "@Cast(\"const char*\") BytePointer"))
+               .put(new Info("std::size_t").cast().valueTypes("long").pointerTypes("LongPointer", "LongBuffer", "long[]"))
+               .put(new Info("TRITONSERVER_EXPORT", "TRITONSERVER_DECLSPEC",
+                             "TRITONBACKEND_DECLSPEC", "TRITONBACKEND_ISPEC",
+                             "TRITONREPOAGENT_DECLSPEC", "TRITONREPOAGENT_ISPEC").cppTypes().annotations())
+        ;
+    }
+}
---
+++ tritonserverwrapper/src/main/java9/module-info.java
@@ -0,0 +1,6 @@
+module org.bytedeco.tritonserverwrapper {
+  requires transitive org.bytedeco.javacpp;
+  exports org.bytedeco.tritonserverwrapper.global;
+  exports org.bytedeco.tritonserverwrapper.presets;
+  exports org.bytedeco.tritonserverwrapper;
+}
