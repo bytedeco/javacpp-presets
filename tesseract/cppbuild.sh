5e9e2bd210e8eba32026618b4d59b0602ca3dbc0
Samuel Audet <samuel.audet@gmail.com>
2022-12-23 21:03:26 +0900

    * Upgrade presets for Leptonica 1.83.0, Tesseract 5.3.0

--- tesseract/cppbuild.sh
+++ tesseract/cppbuild.sh
@@ -7,7 +7,7 @@ if [[ -z "$PLATFORM" ]]; then
     exit
 fi
 
-TESSERACT_VERSION=5.2.0
+TESSERACT_VERSION=5.3.0
 download https://github.com/tesseract-ocr/tesseract/archive/$TESSERACT_VERSION.tar.gz tesseract-$TESSERACT_VERSION.tar.gz
 
 mkdir -p $PLATFORM
