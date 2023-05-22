9152ff6cd440509f5b983a5c69fa13708cb5cf0c
Hervé Guillemet <hg@apteryx.fr>
2023-05-22 23:15:57 +0200

    Add missing exports in module-info

--- pytorch/src/main/java9/module-info.java
+++ pytorch/src/main/java9/module-info.java
@@ -3,5 +3,6 @@ module org.bytedeco.pytorch {
   requires transitive org.bytedeco.openblas;
   exports org.bytedeco.pytorch.global;
   exports org.bytedeco.pytorch.presets;
+  exports org.bytedeco.pytorch.functions;
   exports org.bytedeco.pytorch;
 }
