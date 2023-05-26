a4690b10cf29a4f3fa2d95e574f45ef87b96db6d
Katherine Yang <katheriney@nvidia.com>
2023-05-25 20:27:16 -0700

    renamed tritonserverwrapper to tritondevelopertoolsserver

---
+++ tritondevelopertoolsserver/src/main/java9/module-info.java
@@ -0,0 +1,6 @@
+module org.bytedeco.tritondevelopertoolsserver {
+  requires transitive org.bytedeco.javacpp;
+  exports org.bytedeco.tritondevelopertoolsserver.global;
+  exports org.bytedeco.tritondevelopertoolsserver.presets;
+  exports org.bytedeco.tritondevelopertoolsserver;
+}
