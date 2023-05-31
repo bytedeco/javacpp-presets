03246672e39e00682202355dd7c405d02690883b
devjeonghwan <dev.parkjeonghwan@gmail.com>
2023-05-31 22:56:08 +0900

    * Add new `SampleJpegEncoder` code for nvJPEG module of CUDA (pull #1365)

--- nvcodec/src/main/java/org/bytedeco/nvcodec/presets/nvencodeapi.java
+++ nvcodec/src/main/java/org/bytedeco/nvcodec/presets/nvencodeapi.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2021-2023 Park JeongHwan, Samuel Audet
+ * Copyright (C) 2021-2023 Jeonghwan Park, Samuel Audet
  *
  * Licensed either under the Apache License, Version 2.0, or (at your option)
  * under the terms of the GNU General Public License as published by
@@ -32,7 +32,7 @@ import org.bytedeco.nvcodec.presets.*;
 
 /**
  *
- * @author Park JeongHwan
+ * @author Jeonghwan Park
  */
 @Properties(
     inherit = nvcuvid.class,
