0efe1067413c3fa259908211c28990b291c97c8a
Katherine Yang <katheriney@nvidia.com>
2023-06-21 19:18:43 -0700

    update build script

--- tritonserver/cppbuild.sh
+++ tritonserver/cppbuild.sh
@@ -7,27 +7,35 @@ if [[ -z "$PLATFORM" ]]; then
     exit
 fi
 
-INCLUDE_DEVELOPER_TOOLS_SERVER=${INCLUDE_DEVELOPER_TOOLS_SERVER:=1}
+INCLUDE_DEVELOPER_TOOLS_SERVER=${INCLUDE_DEVELOPER_TOOLS_SERVER:=0}
 
-if [[ ! -f "/opt/tritonserver/include/triton/developer_tools/generic_server_wrapper.h" ]] && [[ ! -f "/opt/tritonserver/lib/libtritondevelopertoolsserver.so" ]] && [ ${INCLUDE_DEVELOPER_TOOLS_SERVER} -eq 1 ]; then
+if [[ ! -f "/opt/tritonserver/include/triton/developer_tools/generic_server_wrapper.h" ]] && [[ ! -f "/opt/tritonserver/lib/libtritondevelopertoolsserver.so" ]] && [[ ${INCLUDE_DEVELOPER_TOOLS_SERVER} -eq 0 ]]; then
     TOOLS_BRANCH=${TOOLS_BRANCH:="https://github.com/triton-inference-server/developer_tools.git"}
-    TOOLS_BRANCH_TAG=${TOOLS_BRANCH_TAG:="main"}
+    TOOLS_BRANCH_TAG=${TOOLS_BRANCH_TAG:="r23.05"}
+    TRITON_CORE_REPO=${TRITON_CORE_REPO:="https://github.com/triton-inference-server/core.git"}
+    TRITON_CORE_REPO_TAG=${TRITON_CORE_REPO_TAG="r23.05"}
     TRITON_HOME="/opt/tritonserver"
     BUILD_HOME="$PWD"/tritonbuild
     mkdir -p ${BUILD_HOME} && cd ${BUILD_HOME}
+    git clone --single-branch --depth=1 -b ${TRITON_CORE_REPO} ${TRITON_CORE_REPO_TAG}
+    cp core/include/triton/core/* ${TRITON_HOME}/include/triton/core/.
+    
+    # Build CPP library and copy over dependencies
     git clone --single-branch --depth=1 -b ${TOOLS_BRANCH_TAG} ${TOOLS_BRANCH}
     cd developer_tools/server
     mkdir build && cd build
-    cmake -DCMAKE_INSTALL_PREFIX:PATH=`pwd`/install -DTRITON_BUILD_TEST=ON -DTRITON_ENABLE_EXAMPLES=ON -DTRITON_BUILD_STATIC_LIBRARY=OFF ..
+    cmake -DCMAKE_INSTALL_PREFIX:PATH=`pwd`/install -DTRITON_BUILD_TEST=ON -DTRITON_ENABLE_EXAMPLES=ON -DTRITON_CORE_REPO_TAG=${TRITON_CORE_REPO_TAG} -DTRITON_BUILD_STATIC_LIBRARY=OFF ..
     make -j"$(grep -c ^processor /proc/cpuinfo)" install
     # Copy dynamic library to triton home
-    cp ${BUILD_HOME}/developer_tools/server/build/install/lib/libtritondevelopertoolsserver.so ${TRITON_HOME}/lib/.
-
-    mkdir -p ${TRITON_HOME}/include/triton/developer_tools/src
-    cp ${BUILD_HOME}/developer_tools/server/include/triton/developer_tools/common.h ${TRITON_HOME}/include/triton/developer_tools/.
-    cp ${BUILD_HOME}/developer_tools/server/include/triton/developer_tools/generic_server_wrapper.h ${TRITON_HOME}/include/triton/developer_tools/.
-    cp ${BUILD_HOME}/developer_tools/server/src/infer_requested_output.h ${TRITON_HOME}/include/triton/developer_tools/src/.
-    cp ${BUILD_HOME}/developer_tools/server/src/tracer.h ${TRITON_HOME}/include/triton/developer_tools/src/
+    cp ${BUILD_HOME}/developer_tools/server/build/libtritondevelopertoolsserver.so ${TRITON_HOME}/lib/.
+    BUILD_INCLUDE_REPO=${BUILD_HOME}/developer_tools/server/include/triton/developer_tools
+    BUILD_SRC_REPO=${BUILD_HOME}/developer_tools/server/src
+    TRITON_INCLUDE_REPO=${TRITON_HOME}/include/triton/developer_tools
+    mkdir -p ${TRITON_INCLUDE_REPO}/src
+    cp ${BUILD_INCLUDE_REPO}/common.h ${TRITON_INCLUDE_REPO}/.
+    cp ${BUILD_INCLUDE_REPO}/generic_server_wrapper.h ${TRITON_INCLUDE_REPO}/.
+    cp ${BUILD_SRC_REPO}/infer_requested_output.h ${TRITON_INCLUDE_REPO}/src/.
+    cp ${BUILD_SRC_REPO}/tracer.h ${TRITON_INCLUDE_REPO}/src/.
     cd ${BUILD_HOME}/..
     rm -r ${BUILD_HOME}
 fi
