819bee5c407aac9c192023c38b9584b1d033dc49
devjeonghwan <dev.parkjeonghwan@gmail.com>
2025-10-01 17:29:48 +0900

    Improve `cppbuild.sh` of onnx presets for cross compile

--- onnx/cppbuild.sh
+++ onnx/cppbuild.sh
@@ -7,122 +7,174 @@ if [[ -z "$PLATFORM" ]]; then
     exit
 fi
 
-# to build with "Traditional ML" support. Untested.
-export ONNX_ML=1
-export CMAKE_BUILD_DIR=.setuptools-cmake-build/
-export MAX_JOBS=$MAKEJ
+export MAX_JOBS=${MAKEJ:-4}
 
 export ONNX=1.19.0
 export PROTO=3.20.3
-export PYBIND=2.11.0
 
 download https://github.com/onnx/onnx/archive/v$ONNX.tar.gz onnx-$ONNX.tar.gz
 download https://github.com/google/protobuf/releases/download/v$PROTO/protobuf-cpp-$PROTO.tar.gz protobuf-$PROTO.tar.gz
-download https://github.com/pybind/pybind11/archive/v$PYBIND.tar.gz pybind11-$PYBIND.tar.gz
 
 mkdir -p "$PLATFORM"
 cd "$PLATFORM"
-INSTALL_PATH=`pwd`
+INSTALL_PATH="$(pwd)"
 
 echo "Decompressing archives..."
 tar --totals -xf ../onnx-$ONNX.tar.gz
 tar --totals -xf ../protobuf-$PROTO.tar.gz
-tar --totals -xf ../pybind11-$PYBIND.tar.gz
 
 export LIBRARY_PATH="$INSTALL_PATH/lib"
 export PATH="$INSTALL_PATH/bin:$PATH"
 export CFLAGS="-I$INSTALL_PATH/include"
 export CXXFLAGS="-I$INSTALL_PATH/include"
-export PYTHON_BIN_PATH=$(which python3)
-if [[ $PLATFORM == windows* ]]; then
-    export PYTHON_BIN_PATH=$(which python.exe)
-fi
+export TARGET_CMAKE_ARGS=""
 
-CPYTHON_PATH="$INSTALL_PATH/../../../cpython/cppbuild/$PLATFORM/"
-
-if [[ -n "${BUILD_PATH:-}" ]]; then
-    PREVIFS="$IFS"
-    IFS="$BUILD_PATH_SEPARATOR"
-    for P in $BUILD_PATH; do
-        if [[ $(find "$P" -name Python.h) ]]; then
-            CPYTHON_PATH="$P"
-        fi
-    done
-    IFS="$PREVIFS"
-fi
-
-CPYTHON_PATH="${CPYTHON_PATH//\\//}"
-
-if [[ -f "$CPYTHON_PATH/include/python3.13/Python.h" ]]; then
-    export LD_LIBRARY_PATH="$CPYTHON_PATH/lib/"
-    export PYTHON_BIN_PATH="$CPYTHON_PATH/bin/python3.13"
-    export PYTHON_INCLUDE_PATH="$CPYTHON_PATH/include/python3.13/"
-    export PYTHON_LIB_PATH="$CPYTHON_PATH/lib/python3.13/"
-    export PYTHON_INSTALL_PATH="$INSTALL_PATH/lib/python3.13/site-packages/"
-    export SSL_CERT_FILE="$CPYTHON_PATH/lib/python3.13/site-packages/pip/_vendor/certifi/cacert.pem"
-    chmod +x $CPYTHON_PATH/bin/*
-elif [[ -f "$CPYTHON_PATH/include/Python.h" ]]; then
-    CPYTHON_PATH=$(cygpath $CPYTHON_PATH)
-    export PATH="$CPYTHON_PATH:$PATH"
-    export PYTHON_BIN_PATH="$CPYTHON_PATH/bin/python.exe"
-    export PYTHON_INCLUDE_PATH="$CPYTHON_PATH/include/"
-    export PYTHON_LIB_PATH="$CPYTHON_PATH/lib/"
-    export PYTHON_INSTALL_PATH="$INSTALL_PATH/lib/site-packages/"
-    export SSL_CERT_FILE="$CPYTHON_PATH/lib/pip/_vendor/certifi/cacert.pem"
-fi
-export PYTHONPATH="$PYTHON_INSTALL_PATH"
-mkdir -p "$PYTHON_INSTALL_PATH"
-
-case $PLATFORM in
-    linux-*)
-        export CC="gcc"
-        export CXX="g++"
-        export CFLAGS="$CFLAGS -fPIC"
-        export CXXFLAGS="$CXXFLAGS -fPIC"
-        ;;
-    macosx-*)
-        export CC="clang"
-        export CXX="clang++"
-        export CFLAGS="$CFLAGS -fPIC"
-        export CXXFLAGS="$CXXFLAGS -fPIC"
-        ;;
-    windows-*)
-        export CC="cl.exe"
-        export CXX="cl.exe"
-        export CMAKE_GENERATOR="Ninja"
-        export USE_MSVC_STATIC_RUNTIME=0
-        ;;
+case "$PLATFORM" in
+  linux-arm64)
+    export CC="aarch64-linux-gnu-gcc"
+    export CXX="aarch64-linux-gnu-g++"
+    export AR="aarch64-linux-gnu-ar"
+    export RANLIB="aarch64-linux-gnu-ranlib"
+    export STRIP="aarch64-linux-gnu-strip"
+    export CFLAGS="$CFLAGS -fPIC"
+    export CXXFLAGS="$CXXFLAGS -fPIC"
+    export TARGET_CMAKE_ARGS="-DCMAKE_SYSTEM_NAME=Linux -DCMAKE_SYSTEM_PROCESSOR=aarch64"
+    ;;
+  linux-*)
+    export CC="gcc"
+    export CXX="g++"
+    export CFLAGS="$CFLAGS -fPIC"
+    export CXXFLAGS="$CXXFLAGS -fPIC"
+    ;;
+  macosx-*)
+    export CC="clang"
+    export CXX="clang++"
+    export CFLAGS="$CFLAGS -fPIC"
+    export CXXFLAGS="$CXXFLAGS -fPIC"
+    ;;
+  windows-*)
+    export CC="cl.exe"
+    export CXX="cl.exe"
+    export CMAKE_GENERATOR="Ninja"
+    export USE_MSVC_STATIC_RUNTIME=0
+    ;;
 esac
 
+####################
+# Compile Protobuf #
+####################
 cd protobuf-$PROTO
-"$CMAKE" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$INSTALL_PATH" -DCMAKE_INSTALL_LIBDIR="lib" -Dprotobuf_MSVC_STATIC_RUNTIME=OFF -Dprotobuf_BUILD_SHARED_LIBS=OFF -Dprotobuf_BUILD_TESTS=OFF cmake
-"$CMAKE" --build . --parallel $MAKEJ --config Release
-"$CMAKE" --install . --config Release
 
-cd ../onnx-$ONNX
-rm -Rf third_party/pybind11 || true
-ln -sf $INSTALL_PATH/pybind11-$PYBIND third_party/pybind11  || true
+# Build `protoc` for Host
+rm -rf _build-host
+mkdir -p _build-host
+cd _build-host
+
+CC= CXX= ${CMAKE:-cmake} \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_INSTALL_PREFIX="$INSTALL_PATH/host" \
+  -DCMAKE_INSTALL_LIBDIR="lib" \
+  -Dprotobuf_BUILD_TESTS=OFF \
+  -Dprotobuf_BUILD_SHARED_LIBS=OFF \
+  -Dprotobuf_MSVC_STATIC_RUNTIME=OFF \
+  ../cmake
+
+${CMAKE:-cmake} --build . --parallel "$MAX_JOBS" --config Release
+${CMAKE:-cmake} --install . --config Release
+
+export PROTOC="$INSTALL_PATH/host/bin/protoc"
+cd ../
+
+# Build `libprotobuf` for target
+rm -rf _build-target
+mkdir -p _build-target
+cd _build-target
+
+${CMAKE:-cmake} \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_C_FLAGS="$CFLAGS" \
+  -DCMAKE_CXX_FLAGS="$CXXFLAGS" \
+  -DCMAKE_INSTALL_PREFIX="$INSTALL_PATH" \
+  -DCMAKE_INSTALL_LIBDIR="lib" \
+  -Dprotobuf_MSVC_STATIC_RUNTIME=OFF \
+  -Dprotobuf_BUILD_SHARED_LIBS=OFF \
+  -Dprotobuf_BUILD_TESTS=OFF \
+  -Dprotobuf_BUILD_PROTOC_BINARIES=OFF \
+  -DProtobuf_PROTOC_EXECUTABLE="$PROTOC" \
+  $TARGET_CMAKE_ARGS \
+  ../cmake
+
+${CMAKE:-cmake} --build . --parallel "$MAX_JOBS" --config Release
+${CMAKE:-cmake} --install . --config Release
+export PROTOBUF_TARGET_INCLUDE="$INSTALL_PATH/include"
+export PROTOBUF_TARGET_STATIC_LIB="$INSTALL_PATH/lib/libprotobuf.a"
+export PROTOBUF_TARGET_LITE_STATIC_LIB="$INSTALL_PATH/lib/libprotobuf-lite.a"
+cd ../
+cd ../
+
+
+################
+# Compile ONNX #
+################
+cd onnx-$ONNX
+
 # work around issue in Xcode's version of Clang, options unsupported by Ninja, and test requirements
-sedinplace 's/const std::string /std::string /g' onnx/defs/schema.h
-sedinplace 's/if WINDOWS:/if False:/g' setup.py
-sedinplace '/if platform.architecture/{N;N;N;N;d;}' setup.py
-sedinplace "/setup_requires.append('pytest-runner')/d" setup.py
-export CMAKE_ARGS="-DCMAKE_INSTALL_PREFIX=$INSTALL_PATH -DONNX_USE_PROTOBUF_SHARED_LIBS=OFF -DProtobuf_USE_STATIC_LIBS=ON -DONNX_USE_LITE_PROTO=ON"
-$PYTHON_BIN_PATH -m pip install --target=$PYTHON_LIB_PATH setuptools==67.6.1 protobuf==$PROTO
-$PYTHON_BIN_PATH -m pip install -e . || true
+sedinplace 's/const std::string /std::string /g' onnx/defs/schema.h || true
 
-mkdir -p ../include/onnx ../include/onnx/common ../include/onnx/defs ../include/onnx/optimizer/ ../include/onnx/optimizer/passes ../include/onnx/version_converter ../include/onnx/version_converter/adapters ../include/onnx/shape_inference
+rm -rf _build
+mkdir -p _build
+cd _build
+ONNX_BUILD_PATH="$(pwd)"
 
-cp onnx/*.h ../include/onnx/
-cp onnx/common/*.h ../include/onnx/common/
-cp onnx/defs/*.h ../include/onnx/defs/
-#cp onnx/optimizer/*.h ../include/onnx/optimizer/
-#cp onnx/optimizer/passes/*.h ../include/onnx/optimizer/passes/
-cp onnx/version_converter/*.h ../include/onnx/version_converter/
-cp onnx/version_converter/adapters/*.h ../include/onnx/version_converter/adapters/
-cp onnx/shape_inference/*.h ../include/onnx/shape_inference/
-cp $CMAKE_BUILD_DIR/onnx/*.h ../include/onnx/
-cp $CMAKE_BUILD_DIR/libonnx* ../lib || true
-cp $CMAKE_BUILD_DIR/onnx*.{lib,dll} ../lib || true
+${CMAKE:-cmake} \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_C_FLAGS="$CFLAGS" \
+  -DCMAKE_CXX_FLAGS="$CXXFLAGS" \
+  -DCMAKE_PREFIX_PATH="$INSTALL_PATH" \
+  -DCMAKE_INSTALL_PREFIX="$INSTALL_PATH" \
+  -DCMAKE_INSTALL_LIBDIR="lib" \
+  -DONNX_ML=ON \
+  -DBUILD_SHARED_LIBS=ON \
+  -DONNX_BUILD_TESTS=OFF \
+  -DONNX_WERROR=OFF \
+  -DONNX_USE_LITE_PROTO=ON \
+  -DONNX_USE_PROTOBUF_SHARED_LIBS=OFF \
+  -DProtobuf_DIR="$INSTALL_PATH/lib/cmake/protobuf" \
+  -DProtobuf_USE_STATIC_LIBS=ON \
+  -DProtobuf_PROTOC_EXECUTABLE="$PROTOC" \
+  -DProtobuf_INCLUDE_DIR="$PROTOBUF_TARGET_INCLUDE" \
+  -DProtobuf_LIBRARY="$PROTOBUF_TARGET_STATIC_LIB" \
+  -DProtobuf_LITE_LIBRARY="$PROTOBUF_TARGET_LITE_STATIC_LIB" \
+  $TARGET_CMAKE_ARGS \
+  ../
 
-cd ../..
+${CMAKE:-cmake} --build . --parallel "$MAX_JOBS" --config Release
+${CMAKE:-cmake} --install . --config Release
+cd ../
+cd ../
+
+
+mkdir -p "$INSTALL_PATH/include/onnx" \
+         "$INSTALL_PATH/include/onnx/common" \
+         "$INSTALL_PATH/include/onnx/defs" \
+         "$INSTALL_PATH/include/onnx/version_converter" \
+         "$INSTALL_PATH/include/onnx/version_converter/adapters" \
+         "$INSTALL_PATH/include/onnx/shape_inference" \
+         "$INSTALL_PATH/lib"
+
+cp -a onnx-$ONNX/onnx/*.h "$INSTALL_PATH/include/onnx/" || true
+cp -a onnx-$ONNX/onnx/common/*.h "$INSTALL_PATH/include/onnx/common/" || true
+cp -a onnx-$ONNX/onnx/defs/*.h "$INSTALL_PATH/include/onnx/defs/" || true
+cp -a onnx-$ONNX/onnx/version_converter/*.h "$INSTALL_PATH/include/onnx/version_converter/" || true
+cp -a onnx-$ONNX/onnx/version_converter/adapters/*.h "$INSTALL_PATH/include/onnx/version_converter/adapters/" || true
+cp -a onnx-$ONNX/onnx/shape_inference/*.h "$INSTALL_PATH/include/onnx/shape_inference/" || true
+
+cp -a $ONNX_BUILD_PATH/*.h "$INSTALL_PATH/include/onnx/" || true
+cp -a $ONNX_BUILD_PATH/libonnx* "$INSTALL_PATH/lib/" || true
+cp -a $ONNX_BUILD_PATH/*.so* "$INSTALL_PATH/lib/" || true
+cp -a $ONNX_BUILD_PATH/*.dylib "$INSTALL_PATH/lib/" || true
+cp -a $ONNX_BUILD_PATH/*.dll "$INSTALL_PATH/lib/" || true
+cp -a $ONNX_BUILD_PATH/*.lib "$INSTALL_PATH/lib/" || true
+cp -a $ONNX_BUILD_PATH/*.a "$INSTALL_PATH/lib/" || true
+
+cd ../
