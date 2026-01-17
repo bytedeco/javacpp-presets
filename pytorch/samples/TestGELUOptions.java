f491e9989ec992bc685e85f81a70538114ab8e30
Samuel Audet <samuel.audet@gmail.com>
2026-01-17 13:47:02 +0900

    Update CHANGELOG.md and fix nits

--- pytorch/samples/TestGELUOptions.java
+++ pytorch/samples/TestGELUOptions.java
@@ -7,20 +7,20 @@ import static org.bytedeco.pytorch.global.torch.*;
 public class TestGELUOptions {
     public static void main(String[] args) {
         System.out.println("Testing GELUOptions.approximate() setter...");
-        
+
         // Test 1: Create GELUOptions with default value
         GELUOptions options1 = new GELUOptions();
         System.out.println("Default approximate: " + options1.approximate().getString());
-        
+
         // Test 2: Use the setter with String (this is the main fix)
         GELUOptions options2 = new GELUOptions().approximate("tanh");
         System.out.println("After setting to 'tanh': " + options2.approximate().getString());
-        
+
         // Test 3: Fluent API chaining should work
         GELUOptions options3 = new GELUOptions()
             .approximate("none");
         System.out.println("After fluent setting to 'none': " + options3.approximate().getString());
-        
+
         // Test 4: Create GELU module with options (as shown in documentation)
         try {
             // This is the usage pattern from the documentation that previously didn't work
