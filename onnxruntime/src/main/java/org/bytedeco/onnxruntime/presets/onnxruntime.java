832647cb2bcffad66a52ebb2b36040c3c9736c97
Dragan Djuric <dragandj@gmail.com>
2026-07-26 10:31:32 +0200

    Define ENABLE_TRAINING and ENABLE_TRAINING_APIS.

--- onnxruntime/src/main/java/org/bytedeco/onnxruntime/presets/onnxruntime.java
+++ onnxruntime/src/main/java/org/bytedeco/onnxruntime/presets/onnxruntime.java
@@ -44,7 +44,8 @@ import org.bytedeco.openvino.presets.*;
         @Platform(
             value = {"linux", "macosx", "windows"},
             compiler = "cpp17",
-            define = {"GENERIC_EXCEPTION_CLASS Ort::Exception", "GENERIC_EXCEPTION_TOSTRING what()"},
+            define = {"GENERIC_EXCEPTION_CLASS Ort::Exception", "GENERIC_EXCEPTION_TOSTRING what()",
+                      "ENABLE_TRAINING", "ENABLE_TRAINING_APIS"},
             include = {
                 "onnxruntime/core/session/onnxruntime_error_code.h",
                 "onnxruntime/core/session/onnxruntime_env_config_keys.h",
